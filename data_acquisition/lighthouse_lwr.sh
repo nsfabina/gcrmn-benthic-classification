7db1594a965d36df62d828ebd52b736161a071f9
Nicholas Fabina <nicholas.fabina@gmail.com>
2019-06-24 12:33:18 -0500

    Updates paths for organizing data.

--- data_acquisition/lighthouse_lwr.sh
+++ data_acquisition/lighthouse_lwr.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-mkdir ../data
+mkdir ../data/lighthouse
 curl https://storage.googleapis.com/coral-atlas-data-share/geojson/lighthouse.geojson \
-  -o ../data/lighthouse.geojson
+  -o ../data/lighthouse/lwr.geojson
--- data_acquisition/lighthouse_mosaics.sh
+++ data_acquisition/lighthouse_mosaics.sh
@@ -2,9 +2,9 @@
 
 URL_ACA=gs://coral-atlas-data-share/coral_reefs_2018_visual_v1_mosaic/caribbean/west-caribbean
 
-mkdir ../data
+mkdir ../data/lighthouse
 for LAT in 0525E 0526E; do
   for LON in 1122N 1123N 1124N 1125N; do
-    gsutil cp $URL_ACA/L15-${LAT}-${LON}.tif ../data
+    gsutil cp $URL_ACA/L15-${LAT}-${LON}.tif ../data/lighthouse/
   done
 done
