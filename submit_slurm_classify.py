30d7263aaad51e1f5845bdcd6b01d3b49cb4438f
nsfabina <nicholas.fabina@gmail.com>
2019-06-26 10:21:33 -0700

    Fixes submission script errors, less restrictive on slurm partition choice.

--- submit_slurm_classify.py
+++ submit_slurm_classify.py
@@ -5,19 +5,19 @@ import subprocess
 
 SLURM_COMMAND = 'sbatch --mail-type=END,FAIL --mail-user=nsfabina@asu.edu --qos=wildfire --time=24:00:00 ' + \
                 '--nodes=1 --cpus-per-task=1 --mem-per-cpu=40000 --gres=gpu:1 --ntasks=1 ' + \
-                '--partition=mrlinegpu1,cidsegpu1,cidsegpu2'
+                '--partition=physicsgpu1,cidsegpu1,sulcgpu1,sulcgpu2,mrlinegpu1,asinghargpu1 '                
 
 SLURM_COMMAND_WRAP = '--wrap "python classify.py --filepath_config=configs/{}"'
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--rerun', type=bool)
+    parser.add_argument('-f', dest='rerun', action='store_true')
     args = parser.parse_args()
 
     filename_configs = ('unet.yaml', )
     for filename_config in filename_configs:
-        job_name = os.path.basename(filename_config)
+        job_name = os.path.splitext(filename_config)[0]
         dir_model = os.path.join('models', job_name)
         if not os.path.exists(dir_model):
             os.makedirs(dir_model)
@@ -27,8 +27,8 @@ if __name__ == '__main__':
         # Set dynamic SLURM arguments
         slurm_args_dynamic = ' '.join([
             '--job-name={}'.format(job_name),
-            '--output={}/slurm.%j.%t.OUT'.format(job_name),
-            '--error={}/slurm.%j.%t.ERROR'.format(job_name),
+            '--output={}/slurm.%j.%t.OUT'.format(dir_model),
+            '--error={}/slurm.%j.%t.ERROR'.format(dir_model),
         ])
         # Set dynamic python arguments
         slurm_python_wrap = SLURM_COMMAND_WRAP.format(filename_config)
