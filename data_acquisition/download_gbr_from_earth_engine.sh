473e1c3201865f9b8aab857bcd5a3f70ce82dc33
Nicholas Fabina <nicholas.fabina@gmail.com>
2019-07-05 12:43:10 -0500

    Adds GBR download script, actually just docs for how to acquire.

---
+++ data_acquisition/download_gbr_from_earth_engine.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+
+echo "You need to download data manually."
+echo "Navigate to https://code.earthengine.google.com/2eb366d331001f344805b35a307a2304 and follow the script provided."
