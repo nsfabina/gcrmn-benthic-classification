a65739c70ecf2f2fe078e3c98a2534cd772d0ef7
Nicholas Fabina <nicholas.fabina@gmail.com>
2019-07-10 10:46:41 -0500

    Fixes pathing.

--- data_acquisition/download_for_application_from_gdrive.sh
+++ data_acquisition/download_for_application_from_gdrive.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 
-if [[ ! -d /scratch/nfabina/gcrmn-benthic-classification/for_application ]]; then
-    mkdir -p /scratch/nfabina/gcrmn-benthic-classification/for_application
+if [[ ! -d ../data/for_application ]]; then
+  mkdir -p ../data/for_application
 fi
 
-rclone copy -v remote:imagery/gcrmn/for_application /scratch/nfabina/gcrmn-benthic-classification/for_application/
+rclone copy -v remote:imagery/gcrmn/for_application ../data/for_application/
