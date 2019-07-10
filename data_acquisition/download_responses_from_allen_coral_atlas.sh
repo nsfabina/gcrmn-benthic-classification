13a7b067907b468a5dbe9582841cd8e8d8b5cd41
Nicholas Fabina <nicholas.fabina@gmail.com>
2019-07-10 12:54:57 -0500

    Updates download script with automated GDrive downloads.

--- data_acquisition/download_responses_from_allen_coral_atlas.sh
+++ data_acquisition/download_responses_from_allen_coral_atlas.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
 
 
-echo "You need to download data manually."
-echo "Navigate to the bookmarked reefs on allencoralatlas.org, click on the pie chart icon, and click 'Download Layers and Stats'."
-echo "The data needs to be unzipped and located in ../data/{reef}/raw/ with folders for each reef location."
+# This data was initially acquired manually from allencoralatlas.org, by navigating to the MVP sites, clicking on the
+# pie chart icon, and clicking 'Download Layers and Stats'. It was subsequently uploaded to nsfabina's GDrive.
+
+
+for REEF in belize hawaii heron karimunjawa moorea; do
+  if [[ ! -d ../data/${REEF}/raw ]]; then
+     mkdir ../data/${REEF}/raw
+
+  rclone copy -v remote:imagery/gcrmn/${REEF}/raw/*.geojson ../data/${REEF}/raw/
