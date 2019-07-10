59aa9e75ce9a0327205c1db9428438ebbdf10d87
Nicholas Fabina <nicholas.fabina@gmail.com>
2019-07-10 10:22:56 -0500

    Fixes paths for application downloads.

--- data_acquisition/download_for_application_from_gdrive.sh
+++ data_acquisition/download_for_application_from_gdrive.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
 
 
-rclone copy -v remote:imagery/gcrmn/for_application /scratch/nfabina/gcrmn-benthic-classification/
+if [[ ! -d /scratch/nfabina/gcrmn-benthic-classification/for_application ]]; then
+    mkdir -p /scratch/nfabina/gcrmn-benthic-classification/for_application
+fi
+
+rclone copy -v remote:imagery/gcrmn/for_application /scratch/nfabina/gcrmn-benthic-classification/for_application/
