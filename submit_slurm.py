cd29bc71c3dc2470135b9da6e688d55a25e4f472
Nicholas Fabina <nicholas.fabina@gmail.com>
2019-07-12 13:14:28 -0500

    Allows flexible config and response parameters.

--- submit_slurm.py
+++ submit_slurm.py
@@ -17,8 +17,9 @@ _OPERATION_ALL = 'all'
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--response_mapping', dest='response_mapping', required=True)
-    parser.add_argument('--operations', dest='operations', required=True)
+    parser.add_argument('--response_mappings', type=str, dest='response_mappings', required=True)
+    parser.add_argument('--operations', type=str, dest='operations', required=True)
+    parser.add_argument('--filepath_configs', type=str, dest='filepath_config')
     parser.add_argument('-f', dest='rerun', action='store_true')
     args = parser.parse_args()
 
@@ -28,34 +29,38 @@ if __name__ == '__main__':
         slurm_command += SLURM_GPUS
 
     # Get relevant configs, only get one config per built data type if building
-    filename_configs = [filename for filename in os.listdir('configs')
-                        if filename.endswith('yaml') and filename != 'config_template.yaml']
-    if args.operations == 'build':
-        filename_configs = [filename for filename in filename_configs if re.search('^unet_\d+_2.yaml', filename)]
+    if args.filepath_config:
+        filename_configs = [os.path.basename(filepath) for filepath in args.filepath_configs.split(',')]
+    else:
+        filename_configs = [filename for filename in os.listdir('configs')
+                            if filename.endswith('yaml') and filename != 'config_template.yaml']
+        if args.operations == 'build':
+            filename_configs = [filename for filename in filename_configs if re.search(r'^unet_\d+_2.yaml', filename)]
 
     # Loop through configs and submit jobs
     for filename_config in filename_configs:
-        config_name = os.path.splitext(filename_config)[0]
-        job_name = config_name + '_' + args.response_mapping
+        for response_mapping in args.response_mappings.split(','):
+            config_name = os.path.splitext(filename_config)[0]
+            job_name = config_name + '_' + args.response_mapping
 
-        # Create model directory or confirm we want to rerun if already exists
-        dir_model = os.path.join('models', config_name, args.response_mapping)
-        if not os.path.exists(dir_model):
-            os.makedirs(dir_model)
-        elif not args.rerun:
-            print('Job {} already submitted, not resubmitting'.format(job_name))
-            continue
+            # Create model directory or confirm we want to rerun if already exists
+            dir_model = os.path.join('models', config_name, args.response_mapping)
+            if not os.path.exists(dir_model):
+                os.makedirs(dir_model)
+            elif not args.rerun:
+                print('Job {} already submitted, not resubmitting'.format(job_name))
+                continue
 
-        # Set dynamic SLURM arguments
-        slurm_args_dynamic = ' '.join([
-            '--job-name={}'.format(job_name),
-            '--output={}/slurm.%j.%t.OUT'.format(dir_model),
-            '--error={}/slurm.%j.%t.ERROR'.format(dir_model),
-        ])
+            # Set dynamic SLURM arguments
+            slurm_args_dynamic = ' '.join([
+                '--job-name={}'.format(job_name),
+                '--output={}/slurm.%j.%t.OUT'.format(dir_model),
+                '--error={}/slurm.%j.%t.ERROR'.format(dir_model),
+            ])
 
-        # Set dynamic python arguments
-        slurm_python_wrap = SLURM_COMMAND_WRAP.format(filename_config, args.response_mapping, args.operations)
+            # Set dynamic python arguments
+            slurm_python_wrap = SLURM_COMMAND_WRAP.format(filename_config, args.response_mapping, args.operations)
 
-        print('Submitting job {}'.format(job_name))
-        command = ' '.join([slurm_command, slurm_args_dynamic, slurm_python_wrap])
-        subprocess.call(command, shell=True)
+            print('Submitting job {}'.format(job_name))
+            command = ' '.join([slurm_command, slurm_args_dynamic, slurm_python_wrap])
+            subprocess.call(command, shell=True)
