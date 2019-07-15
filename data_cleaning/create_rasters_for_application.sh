88bc079bd2737ca2c86fef660ea58d7342d244e8
nsfabina <nicholas.fabina@gmail.com>
2019-07-15 09:55:47 -0700

    Fixes order of window args.

--- data_cleaning/create_rasters_for_application.sh
+++ data_cleaning/create_rasters_for_application.sh
@@ -11,54 +11,54 @@ fi
 # Belize
 
 gdal_translate \
-  -projwin -87.650 17.100 -87.450 17.500 \
+  -projwin -87.650 17.500 -87.450 17.100 \
   ../data/belize/clean/features.vrt \
   ../data/for_application/belize_0.tif
 
 
 # Hawaii
 
-gdal_translate -projwin -155.844 19.970 -155.820 20.0480 \
+gdal_translate -projwin -155.844 20.048 -155.820 19.970 \
   ../data/hawaii/clean/features.vrt \
   ../data/for_application/hawaii_0.tif
 
-gdal_translate -projwin -156.057 19.780 -155.980 19.840 \
+gdal_translate -projwin -156.057 19.840 -155.980 19.780 \
   ../data/hawaii/clean/features.vrt \
   ../data/for_application/hawaii_1.tif
 
-gdal_translate -projwin -155.918 19.876 -155.875 19.947 \
+gdal_translate -projwin -155.918 19.947 -155.875 19.876 \
   ../data/hawaii/clean/features.vrt \
   ../data/for_application/hawaii_2.tif
 
 
 # Heron
 
-gdal_translate -projwin 151.876 -23.511 152.051 -23.403 \
+gdal_translate -projwin 151.876 -23.403 152.051 -23.511 \
   ../data/heron/clean/features.vrt \
   ../data/for_application/heron_0.tif
 
 
 # Karimunjawa
 
-gdal_translate -projwin 110.398 -5.906 110.521 -5.764 \
+gdal_translate -projwin 110.398 -5.764 110.521 -5.906 \
   ../data/karimunjawa/clean/features.vrt \
   ../data/for_application/karimunjawa_0.tif
 
-gdal_translate -projwin 110.116 -5.830 110.276 -5.713 \
+gdal_translate -projwin 110.116 -5.713 110.276 -5.830 \
   ../data/karimunjawa/clean/features.vrt \
   ../data/for_application/karimunjawa_1.tif
 
 
 # Moorea
 
-gdal_translate -projwin -149.929 -17.510 -149.749 -17.466 \
+gdal_translate -projwin -149.929 -17.466 -149.749 -17.510 \
   ../data/moorea/clean/features.vrt \
   ../data/for_application/moorea_0.tif
 
-gdal_translate -projwin -149.891 -17.608 -149.775 -17.560 \
+gdal_translate -projwin -149.891 -17.560 -149.775 -17.608 \
   ../data/moorea/clean/features.vrt \
   ../data/for_application/moorea_1.tif
 
-gdal_translate -projwin -149.593 -17.800 -149.293 -17.734 \
+gdal_translate -projwin -149.593 -17.734 -149.293 -17.800 \
   ../data/moorea/clean/features.vrt \
   ../data/for_application/moorea_2.tif
