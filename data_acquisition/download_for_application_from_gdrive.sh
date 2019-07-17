3df36a29ffbc7653363ed4ae9da7b031ffffd39d
Nicholas Fabina <nicholas.fabina@gmail.com>
2019-07-17 10:46:27 -0500

    Converts for application downloads to full mosaic directory.

--- data_acquisition/download_for_application_from_gdrive.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-
-if [[ ! -d ../data/for_application ]]; then
-  mkdir -p ../data/for_application
-fi
-
-rclone copy -v remote:imagery/gcrmn/for_application ../data/for_application/
