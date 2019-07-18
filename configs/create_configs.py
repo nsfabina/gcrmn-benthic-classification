09681977ea9f9eae3e79598d28296f072e2b6489
Nicholas Fabina <nicholas.fabina@gmail.com>
2019-07-18 09:40:09 -0500

    Modifies which architecture parameters are tested.

--- configs/create_configs.py
+++ configs/create_configs.py
@@ -7,21 +7,20 @@ _FILEPATH_TEMPLATE = 'config_template.yaml'
 def create_configs() -> None:
     config_template = configs.create_config_from_file(_FILEPATH_TEMPLATE)
 
-    for window_radius in (256, 128):
+    for window_radius in (128, ):  #192, 256):
         for architecture_name in ('unet', 'dense_unet'):
-            for block_structure in ([2, 2, 2, 2], [4, 4, 4]):
-                for use_growth in (True, False):
+            for block_structure in ([2, 2, 2, 2], [2, 2, 2], [4, 4, 4], [4, 4]):
+                for filters in (4, 8, 12, 16):
                     # Create new config
                     config_template.data_build.window_radius = window_radius
                     config_template.data_build.loss_window_radius = int(window_radius / 2)
                     config_template.model_training.architecture_name = architecture_name
                     config_template.architecture.block_structure = block_structure
-                    config_template.architecture.use_growth = use_growth
+                    config_template.architecture.filters = filters
 
                     # Save config to file
-                    basename = '{}_{}_{}'.format(architecture_name, window_radius, block_structure[0])
-                    if use_growth is True:
-                        basename += '_growth'
+                    basename = '{}_{}_{}{}_{}'.format(
+                        architecture_name, window_radius, block_structure[0], len(block_structure), filters)
                     filepath = basename + '.yaml'
                     configs.save_config_to_file(config_template, filepath)
 
