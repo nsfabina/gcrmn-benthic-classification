180cfface1d7009e2479403773a0dc33773e1fea
Nicholas Fabina <nicholas.fabina@gmail.com>
2019-07-18 11:16:54 -0500

    Handles build_only more gracefully after needing to update.

--- configs/create_configs.py
+++ configs/create_configs.py
@@ -8,6 +8,7 @@ def create_configs() -> None:
     config_template = configs.create_config_from_file(_FILEPATH_TEMPLATE)
 
     for window_radius in (128, ):  #192, 256):
+        created_build_only = False
         for architecture_name in ('unet', 'dense_unet'):
             for block_structure in ([2, 2, 2, 2], [2, 2, 2], [4, 4, 4], [4, 4]):
                 for filters in (4, 8, 12, 16):
@@ -23,6 +24,11 @@ def create_configs() -> None:
                         architecture_name, window_radius, block_structure[0], len(block_structure), filters)
                     filepath = basename + '.yaml'
                     configs.save_config_to_file(config_template, filepath)
+
+                    # Save config for build only
+                    if not created_build_only:
+                        configs.save_config_to_file(config_template, 'build_only_{}.yaml'.format(window_radius))
+                        created_build_only = True
 
 
 if __name__ == '__main__':
--- submit_slurm.py
+++ submit_slurm.py
@@ -13,24 +13,31 @@ SLURM_COMMAND_WRAP = '--wrap "python run_classification.py --filepath_config=con
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--filepath_configs', type=str)
-    parser.add_argument('--response_mappings', type=str, required=True)
+    parser.add_argument('--response_mappings', type=str)
     parser.add_argument('--build_only', type=bool, action='store_true')
     parser.add_argument('-f', dest='rerun', action='store_true')
     args = parser.parse_args()
+
+    # Warning about usage and error checks
+    if args.build_only and (args.filepaths_configs or args.response_mappings):
+        print('WARNING:  build_only takes precedence over filepaths_configs and response_mappings, which are ignored')
+    assert args.build_only or args.response_mappings, 'response_mappings must be provided if not using build_only'
 
     # Prep commands
     slurm_command = SLURM_COMMAND
     if not args.build_only:
         slurm_command += SLURM_GPUS
 
-    # Get relevant configs, only get one config per built data type if building
-    if args.filepath_configs:
+    # Get relevant configs, only get one config per window radius if building
+    if args.build_only:
+        filename_configs = [filename for filename in os.listdir('configs') if filename.startswith('build_only')]
+    elif args.filepath_configs:
         filename_configs = [os.path.basename(filepath) for filepath in args.filepath_configs.split(',')]
     else:
-        filename_configs = [filename for filename in os.listdir('configs')
-                            if filename.endswith('yaml') and filename != 'config_template.yaml']
-        if args.operations == 'build':
-            filename_configs = [filename for filename in filename_configs if re.search(r'^unet_\d+_2.yaml', filename)]
+        filename_configs = [
+            filename for filename in os.listdir('configs') if filename.endswith('yaml')
+            and filename != 'config_template.yaml' and not filename.startswith('build_only')
+        ]
 
     # Loop through configs and submit jobs
     for filename_config in filename_configs:
