28bbde1f8afcfb6f7ead0fd0004e271792df3560
Nicholas Fabina <nicholas.fabina@gmail.com>
2019-07-23 09:36:32 -0500

    Renames scripts for clarity.

--- data_acquisition/download_all.sh
+++ data_acquisition/download_all.sh
@@ -2,6 +2,6 @@
 
 
 ./download_boundaries_from_fabina_gdrive.sh
-./download_responses_from_fabina_gdrive.sh
+download_mvp_responses_from_fabina_gdrive.sh
 ./download_for_application_from_gcs_vulcan.sh
-./copy_features_from_downloaded_mosaics.sh
+copy_mvp_features_from_downloaded_mosaics.sh
