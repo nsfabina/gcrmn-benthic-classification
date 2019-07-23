2b6113a8543a3397174be01811e0ad7327e844c3
Nicholas Fabina <nicholas.fabina@gmail.com>
2019-07-23 15:27:32 -0500

    Removes fourth band from Vulcan reflectance.

--- data_cleaning/clean_vulcan_formatted_training_data.sh
+++ data_cleaning/clean_vulcan_formatted_training_data.sh
@@ -1,62 +1,68 @@
 #!/usr/bin/env bash
 
 
-DIR_DEST=/scratch/nfabina/gcrmn-benthic-classification/training_data
+DIR_DEST="/scratch/nfabina/gcrmn-benthic-classification/training_data"
 
 
 for REEF in belize hawaii heron karimunjawa moorea; do
     DIR_REEF="${DIR_DEST}/${REEF}"
-    echo "Cleaning data for reef directory: " ${DIR_REEF}
+    echo "Cleaning data for reef directory: ${DIR_REEF}"
 
-    if [[ ! -d ${DIR_REEF}/tmp ]]; then
-        mkdir -p ${DIR_REEF}/tmp
+    DIR_RAW="${DIR_REEF}/raw"
+    DIR_TMP="${DIR_REEF}/tmp"
+    DIR_CLEAN="${DIR_REEF}/clean"
+
+    if [[ ! -d "${DIR_TMP}" ]]; then
+        mkdir -p "${DIR_TMP}"
     fi
 
-    if [[ ! -d ${DIR_REEF}/clean ]]; then
-        mkdir -p ${DIR_REEF}/clean
+    if [[ ! -d "${DIR_CLEAN}" ]]; then
+        mkdir -p "${DIR_CLEAN}"
     fi
 
-    for FILEPATH in ${DIR_REEF}/raw/*.tif; do
+    for FILEPATH in "${DIR_RAW}/*.tif"; do
         FILENAME=$(basename ${FILEPATH})
 
-        if [[ ! -f ${DIR_REEF}/clean/${FILENAME} ]]; then
-            echo "Cleaning data for imagery file: " ${FILEPATH}
+        if [[ ! -f "${DIR_CLEAN}/${FILENAME}" ]]; then
+            echo "Cleaning data for imagery file: ${FILEPATH}"
 
             echo "Reprojecting"
             # Note that mosaics come in different projections than individual scenes
-            gdalwarp -s_srs EPSG:3857 -t_srs EPSG:4326 ${FILEPATH} ${DIR_REEF}/clean/${FILENAME}
+            gdalwarp -s_srs EPSG:3857 -t_srs EPSG:4326 ${FILEPATH} "${DIR_TMP}/${FILENAME}"
+
+            echo "Removing fourth band"
+            gdal_translate -b 1 -b -2 -b -3 "${DIR_TMP}/${FILENAME}" "${DIR_CLEAN}/${FILENAME}"
 
         else
-            echo "Imagery file already cleaned: " ${FILEPATH}
+            echo "Imagery file already cleaned: ${FILEPATH}"
         fi
     done
 
-    if [[ ! -f ${DIR_REEF}/clean/features.vrt ]]; then
+    if [[ ! -f "${DIR_CLEAN}/features.vrt" ]]; then
         echo "Building imagery VRT"
         # Note that it's easier to use a vrt than to assemble paired features/responses manually
-        gdalbuildvrt ${DIR_REEF}/clean/features.vrt ${DIR_REEF}/clean/*.tif
+        gdalbuildvrt "${DIR_CLEAN}/features.vrt" "${DIR_CLEAN}/*.tif"
     else
         echo "Imagery VRT already built"
     fi
 
     # Calculate parameters for responses
-    FILENAME_IN=$(basename ${DIR_REEF}/raw/responses.geojson)
-    DIR_TMP=${DIR_REEF}/tmp
+    FILENAME_IN=$(basename ${DIR_RAW}/responses.geojson)
     PT_REGEX='\-*[0-9]+\.*[0-9]+,( )*\-*[0-9]+\.*[0-9]+'
-    LOWER_LEFT=$(gdalinfo ${DIR_REEF}/clean/features.vrt | grep 'Lower Left' | egrep -o "${PT_REGEX}" | tr -d ',')
-    UPPER_RIGHT=$(gdalinfo ${DIR_REEF}/clean/features.vrt | grep 'Upper Right' | egrep -o "${PT_REGEX}" | tr -d ',')
+    LOWER_LEFT=$(gdalinfo ${DIR_CLEAN}/features.vrt | grep 'Lower Left' | egrep -o "${PT_REGEX}" | tr -d ',')
+    UPPER_RIGHT=$(gdalinfo ${DIR_CLEAN}/features.vrt | grep 'Upper Right' | egrep -o "${PT_REGEX}" | tr -d ',')
     RES_REGEX='[0-9]+\.*[0-9]+,\-[0-9]+\.*[0-9]+'
-    RESOLUTION=$(gdalinfo ${DIR_REEF}/clean/features.vrt | grep 'Pixel Size' | egrep -o "${RES_REGEX}" | tr ',' ' ')
+    RESOLUTION=$(gdalinfo ${DIR_CLEAN}/features.vrt | grep 'Pixel Size' | egrep -o "${RES_REGEX}" | tr ',' ' ')
 
 
     TMP_FILEPATH_OUT="${DIR_TMP}/responses_lwr.geojson"
-    CLEAN_FILEPATH_OUT="${DIR_REEF}/clean/responses_lwr.tif"
+    CLEAN_FILEPATH_OUT="${DIR_CLEAN}/responses_lwr.tif"
     if [[ ! -f ${CLEAN_FILEPATH_OUT} ]]; then
         echo "Cleaning data for LWR models"
 
         # Note that the lwr_class key with string values causes issues with the SQL in rasterization, so we convert
         # that to the lwr key with integer values
-        sed 's/"lwr_class": "Land"/"lwr": 1/g' "${DIR_REEF}/raw/${FILENAME_IN}" > ${TMP_FILEPATH_OUT}
+        sed 's/"lwr_class": "Land"/"lwr": 1/g' "${DIR_RAW}/${FILENAME_IN}" > ${TMP_FILEPATH_OUT}
         sed -i 's/"lwr_class": "Deep Reef Water 10m+"/"lwr": 2/g' ${TMP_FILEPATH_OUT}
         sed -i 's/"lwr_class": "Reef"/"lwr": 3/g' ${TMP_FILEPATH_OUT}
         sed -i 's/"lwr_class": "Cloud[^"]*Shade"/"lwr": -9999/g' ${TMP_FILEPATH_OUT}
