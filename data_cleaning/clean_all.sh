fa3a12ba074adee9bd55190c529f158b7554ef8b
Nicholas Fabina <nicholas.fabina@gmail.com>
2019-07-24 14:00:27 -0500

    Adds script to create Reef/NoReef shapefiles for evaluation.

--- data_cleaning/clean_all.sh
+++ data_cleaning/clean_all.sh
@@ -5,3 +5,4 @@ set -e
 
 ./clean_gbr_training_data.sh
 ./clean_vulcan_formatted_training_data.sh
+./create_performance_evaluation_rasters.sh
---
+++ data_cleaning/create_performance_evaluation_rasters.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+
+set -e
+
+
+DIR_BASE="/scratch/nfabina/gcrmn-benthic-classification/training_data"
+
+
+for REEF in `ls ${DIR_BASE}`; do
+    echo "Creating Reef/NoReef shapefile for ${REEF}"
+
+    DIR_REEF="${DIR_BASE}/${REEF}"
+
+    echo "Creating intermediate Reef/NoReef raster"
+    gdal_calc.py -A "${DIR_REEF}/clean/responses_lwr.tif" --outfile="${DIR_REEF}/tmp/responses_r.tif" \
+        --calc="(A==3)-9999*(A!=3)" --NoDataValue=-9999 --overwrite --quiet
+
+    echo "Creating intermediate Reef/NoReef raster"
+    gdal_polygonize.py "${DIR_REEF}/tmp/responses_r.tif" "${DIR_REEF}/clean/reef_outline.shp" -q
+
+done
