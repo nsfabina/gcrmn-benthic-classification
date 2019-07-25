4616844b64ae24d504af62c75bd31f417b945d8e
Nicholas Fabina <nicholas.fabina@gmail.com>
2019-07-25 11:13:22 -0500

    Updates log message.

--- data_cleaning/create_performance_evaluation_rasters.sh
+++ data_cleaning/create_performance_evaluation_rasters.sh
@@ -16,7 +16,7 @@ for REEF in `ls ${DIR_BASE}`; do
     gdal_calc.py -A "${DIR_REEF}/clean/responses_lwr.tif" --outfile="${DIR_REEF}/tmp/responses_r.tif" \
         --calc="(A==3)-9999*(A!=3)" --NoDataValue=-9999 --type="Float32" --overwrite --quiet
 
-    echo "Creating intermediate Reef/NoReef raster"
+    echo "Creating final Reef/NoReef shapefile"
     gdal_polygonize.py "${DIR_REEF}/tmp/responses_r.tif" "${DIR_REEF}/clean/reef_outline.shp" -q
 
 done
