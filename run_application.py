f776783903c7da42219d9393785bcef9108ee71b
Nicholas Fabina <nicholas.fabina@gmail.com>
2019-07-26 11:31:53 -0500

    Adds application for training data.

--- run_application.py
+++ run_application.py
@@ -13,19 +13,24 @@ import shared_configs
 
 _DIR_CONFIGS = 'configs'
 _DIR_APPLY_BASE = '/scratch/nfabina/gcrmn-benthic-classification'
-_SUBDIR_APPLY_IN = 'visual_mosaic_v1'
-_SUBDIR_APPLY_OUT = 'visual_mosaic_v1_applied/{}/{}'
-_DIR_APPLY_IN = os.path.join(_DIR_APPLY_BASE, _SUBDIR_APPLY_IN)
-_LOG_OUT = os.path.join(_DIR_APPLY_BASE, _SUBDIR_APPLY_OUT, 'log.out')
-_FILE_APPLY_OUT = '_applied.tif'
+
+_SUBDIR_MOSAIC_IN = 'visual_mosaic_v1'
+_SUBDIR_MOSAIC_OUT = 'visual_mosaic_v1_applied/{}/{}'
+_DIR_MOSAIC_IN = os.path.join(_DIR_APPLY_BASE, _SUBDIR_MOSAIC_IN)
+
+_SUBDIR_TRAINING_IN = 'training_data'
+_SUBDIR_TRAINING_OUT = 'training_data_applied/{}/{}'
+_DIR_TRAINING_IN = os.path.join(_DIR_APPLY_BASE, _SUBDIR_MOSAIC_IN)
+
+_FILENAME_SUFFIX_OUT = '_applied.tif'
 
 
-def run_application(config_name: str, response_mapping: str) -> None:
+def run_application_to_training_data(config_name: str, response_mapping: str) -> None:
     filepath_config = os.path.join(_DIR_CONFIGS, config_name + '.yaml')
     config = shared_configs.build_dynamic_config(filepath_config, response_mapping)
 
     # Get paths and logger
-    log_out = _LOG_OUT.format(config_name)
+    log_out = os.path.join(_DIR_APPLY_BASE, _SUBDIR_TRAINING_OUT.format(config_name, response_mapping), 'log.out')
     if not os.path.exists(os.path.dirname(log_out)):
         os.makedirs(os.path.dirname(log_out))
     logger = logging.get_root_logger(log_out)
@@ -41,10 +46,11 @@ def run_application(config_name: str, response_mapping: str) -> None:
     experiment.build_or_load_model(data_container)
 
     # Apply model
-    filepaths_apply = _get_application_raster_filepaths(logger)
-    subdir_out = _SUBDIR_APPLY_OUT.format(config_name, response_mapping)
+    filepaths_apply = _get_application_raster_filepaths_for_training_data(response_mapping, logger)
+    subdir_out = _SUBDIR_TRAINING_OUT.format(config_name, response_mapping)
     for idx_filepath, filepath_apply in enumerate(filepaths_apply):
-        filepath_out = os.path.splitext(re.sub(_SUBDIR_APPLY_IN, subdir_out, filepath_apply))[0] + _FILE_APPLY_OUT
+        filepath_out = \
+            os.path.splitext(re.sub(_SUBDIR_TRAINING_IN, subdir_out, filepath_apply))[0] + _FILENAME_SUFFIX_OUT
         if not os.path.exists(os.path.dirname(filepath_out)):
             os.makedirs(os.path.dirname(filepath_out))
         logger.debug('Applying model to raster {} of {}; input and output filepaths are {} and {}'.format(
@@ -52,9 +58,52 @@ def run_application(config_name: str, response_mapping: str) -> None:
         _apply_to_raster(experiment, data_container, filepath_apply, filepath_out, logger)
 
 
-def _get_application_raster_filepaths(logger: Logger) -> List[str]:
+def _get_application_raster_filepaths_for_training_data(response_mapping: str, logger: Logger) -> List[str]:
     filepaths = list()
-    for path, dirnames, filenames in os.walk(_DIR_APPLY_IN):
+    for path, dirnames, filenames in os.walk(_DIR_TRAINING_IN):
+        for filename in filenames:
+            if filename != 'responses_{}.tif'.format(response_mapping):
+                continue
+            filepaths.append(os.path.join(path, filename))
+    logger.debug('Found {} rasters for application'.format(len(filepaths)))
+    return sorted(filepaths)
+
+
+def run_application_to_global_mosaic(config_name: str, response_mapping: str) -> None:
+    filepath_config = os.path.join(_DIR_CONFIGS, config_name + '.yaml')
+    config = shared_configs.build_dynamic_config(filepath_config, response_mapping)
+
+    # Get paths and logger
+    log_out = os.path.join(_DIR_APPLY_BASE, _SUBDIR_MOSAIC_OUT.format(config_name, response_mapping), 'log.out')
+    if not os.path.exists(os.path.dirname(log_out)):
+        os.makedirs(os.path.dirname(log_out))
+    logger = logging.get_root_logger(log_out)
+
+    # Build dataset
+    data_container = data_core.DataContainer(config)
+    data_container.build_or_load_rawfile_data()
+    data_container.build_or_load_scalers()
+    data_container.load_sequences()
+
+    # Build experiment
+    experiment = experiments.Experiment(config)
+    experiment.build_or_load_model(data_container)
+
+    # Apply model
+    filepaths_apply = _get_application_raster_filepaths_for_global_mosaics(logger)
+    subdir_out = _SUBDIR_MOSAIC_OUT.format(config_name, response_mapping)
+    for idx_filepath, filepath_apply in enumerate(filepaths_apply):
+        filepath_out = os.path.splitext(re.sub(_SUBDIR_MOSAIC_IN, subdir_out, filepath_apply))[0] + _FILENAME_SUFFIX_OUT
+        if not os.path.exists(os.path.dirname(filepath_out)):
+            os.makedirs(os.path.dirname(filepath_out))
+        logger.debug('Applying model to raster {} of {}; input and output filepaths are {} and {}'.format(
+            idx_filepath+1, len(filepath_apply), filepath_apply, filepath_out))
+        _apply_to_raster(experiment, data_container, filepath_apply, filepath_out, logger)
+
+
+def _get_application_raster_filepaths_for_global_mosaics(logger: Logger) -> List[str]:
+    filepaths = list()
+    for path, dirnames, filenames in os.walk(_DIR_MOSAIC_IN):
         for filename in filenames:
             if not filename.endswith('.tif'):
                 continue
@@ -90,7 +139,7 @@ def _apply_to_raster(
     # Apply model to raster and clean up file lock
     try:
         apply_model_to_data.apply_model_to_raster(
-            experiment.model, data_container, filepath_apply, basename_out, exclude_feature_nodata=True)
+            experiment.model, data_container, [filepath_apply], basename_out, exclude_feature_nodata=True)
         logger.debug('Application success, removing lock file')
     except Exception as error_:
         raise error_
@@ -102,7 +151,13 @@ def _apply_to_raster(
 
 if __name__ == '__main__':
     parser = ArgumentParser()
+    parser.add_argument('--target', required=True)
     parser.add_argument('--config_name', required=True)
     parser.add_argument('--response_mapping', required=True)
-    args = vars(parser.parse_args())
-    run_application(**args)
+    args = parser.parse_args()
+    if args.target == 'training':
+        run_application_to_training_data(args.config_name, args.response_mapping)
+    elif args.target == 'mosaic':
+        run_application_to_global_mosaic(args.config_name, args.response_mapping)
+    else:
+        raise AssertionError('target must be either training or mosaic')
