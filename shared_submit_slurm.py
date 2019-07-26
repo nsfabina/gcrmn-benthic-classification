39c23efb46f2cbcfd83c5117a30420f912582e0a
nsfabina <nicholas.fabina@gmail.com>
2019-07-26 12:51:08 -0700

    Lowers SLURM time to address timeouts more quickly.

--- shared_submit_slurm.py
+++ shared_submit_slurm.py
@@ -1,4 +1,4 @@
-SLURM_COMMAND = 'sbatch --mail-type=FAIL --mail-user=nsfabina@asu.edu --time=24:00:00 ' + \
+SLURM_COMMAND = 'sbatch --mail-type=FAIL --mail-user=nsfabina@asu.edu --time=4:00:00 ' + \
                 '--nodes=1 --cpus-per-task=1 --mem-per-cpu=20000 --ntasks=1 '
 SLURM_GPUS_LARGE = '--qos=wildfire --gres=gpu:1 --partition=mrlinegpu1,rcgpu1 '
 SLURM_GPUS = '--qos=wildfire --gres=gpu:1 ' + \
