8b57de1372054d19010ddd12d04ba2c87ed975aa
Nicholas Fabina <nicholas.fabina@gmail.com>
2019-07-29 09:54:57 -0500

    Adds script to create model comparison report.

---
+++ compare_models.py
@@ -0,0 +1,27 @@
+import os
+
+from bfgn.reporting import comparisons
+
+
+_DIR_MODELS = 'models'
+_RESPONSE_MAPPING = 'lwr'
+
+
+def compare_models() -> None:
+    paths_histories_started = comparisons.walk_directories_for_model_histories([_DIR_MODELS])
+    paths_histories_complete = list()
+    for path_history in paths_histories_started:
+        if _RESPONSE_MAPPING not in path_history:
+            continue
+        path_success = os.path.join(os.path.dirname(path_history), 'classify.success')
+        if os.path.exists(path_success):
+            paths_histories_complete.append(path_history)
+
+    comparisons.create_model_comparison_report(
+        filepath_out=os.path.join(_DIR_MODELS, 'comparison_report_{}.pdf'.format(_RESPONSE_MAPPING)),
+        paths_histories=paths_histories_complete
+    )
+
+
+if __name__ == '__main__':
+    compare_models()
