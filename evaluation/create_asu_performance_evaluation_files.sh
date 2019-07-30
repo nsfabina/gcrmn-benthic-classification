8487bd8174bea30e03c3083d36718473e9c22c5f
Nicholas Fabina <nicholas.fabina@gmail.com>
2019-07-30 12:43:33 -0500

    Addresses OOM in gdal_calc.

--- evaluation/create_asu_performance_evaluation_files.sh
+++ evaluation/create_asu_performance_evaluation_files.sh
@@ -32,7 +32,9 @@ for REEF in `ls "${DIR_BASE}"`; do
     for RASTER in `ls ${DIR_REEF}/*applied.tif`; do
         FILENAME="$(basename $(basename $RASTER) .tif)"
         BASENAME="${FILENAME%_*}"
-        FILEPATH_OUT_TMP="${DIR_REEF}/tmp/${BASENAME}_reefs.tif"
+        FILEPATH_TMP_1="${DIR_REEF}/tmp/${BASENAME}_1.tif"
+        FILEPATH_TMP_2="${DIR_REEF}/tmp/${BASENAME}_2.tif"
+        FILEPATH_TMP_3="${DIR_REEF}/tmp/${BASENAME}_reefs.tif"
         FILEPATH_OUT_CLEAN="${DIR_REEF}/${BASENAME}_reef_outline.shp"
 
         printf "\nCreating Reef/NoReef shapefile for raster: \n${RASTER}\n"
@@ -43,13 +45,22 @@ for REEF in `ls "${DIR_BASE}"`; do
             continue
         fi
 
-        printf "Creating intermediate Reef/NoReef raster at: \n${FILEPATH_OUT_TMP}\n"
-        gdal_calc.py -A "${RASTER}" --A_band=1 -B "${RASTER}" --B_band=2 -C "${RASTER}" --C_band=3 \
-            --outfile="${FILEPATH_OUT_TMP}" --NoDataValue=-9999 --type="Float32" --overwrite --quiet \
-            --calc="-9999 + 10000 * numpy.logical_and(C > A, C > B)"
+        printf "Creating intermediate Reef/NoReef raster at: \n${FILEPATH_TMP_3}\n"
+        # Reefs more likely than land
+        gdal_calc.py -A "${RASTER}" --A_band=3 -B "${RASTER}" --B_band=1 \
+            --outfile="${FILEPATH_TMP_1}" --NoDataValue=-9999 --type="Float32" --overwrite --quiet \
+            --calc="A > B"
+        # Reefs more likely than water
+        gdal_calc.py -A "${RASTER}" --A_band=3 -B "${RASTER}" --B_band=2 \
+            --outfile="${FILEPATH_TMP_2}" --NoDataValue=-9999 --type="Float32" --overwrite --quiet \
+            --calc="A > B"
+        # Reefs more likely in both
+        gdal_calc.py -A "${FILEPATH_TMP_1}" "${FILEPATH_TMP_2}" \
+            --outfile="${FILEPATH_TMP_3}" --NoDataValue=-9999 --type="Float32" --overwrite --quiet \
+            --calc="numpy.logical_and(A, B)"
 
         printf "Creating final Reef/NoReef shapefile at: \n${FILEPATH_OUT_CLEAN}\n"
-        gdal_polygonize.py "${FILEPATH_OUT_TMP}" "${FILEPATH_OUT_CLEAN}" -q
+        gdal_polygonize.py "${FILEPATH_TMP_3}" "${FILEPATH_OUT_CLEAN}" -q
     done
 
 done
