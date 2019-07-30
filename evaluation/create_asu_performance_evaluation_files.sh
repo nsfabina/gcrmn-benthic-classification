4ac0076c3d6d07a03e1a32e2d42aefd8d9c72a4b
Nicholas Fabina <nicholas.fabina@gmail.com>
2019-07-30 11:38:59 -0500

    Fixes intermediate raster calculation.

--- evaluation/create_asu_performance_evaluation_files.sh
+++ evaluation/create_asu_performance_evaluation_files.sh
@@ -44,8 +44,9 @@ for REEF in `ls "${DIR_BASE}"`; do
         fi
 
         printf "Creating intermediate Reef/NoReef raster at: \n${FILEPATH_OUT_TMP}\n"
-        gdal_calc.py -A "${RASTER}" --outfile="${FILEPATH_OUT_TMP}" \
-            --calc="(A==3)-9999*(A!=3)" --NoDataValue=-9999 --type="Float32" --overwrite --quiet
+        gdal_calc.py -A "${RASTER}" --A_band=1 -B "${RASTER}" --B_band=2 -C "${RASTER}" --C_band=3 \
+            --outfile="${FILEPATH_OUT_TMP}" --NoDataValue=-9999 --type="Float32" --overwrite --quiet
+            --calc="-9999 + 10000 * numpy.logical_and(C > A, C > B)"
 
         printf "Creating final Reef/NoReef shapefile at: \n${FILEPATH_OUT_CLEAN}\n"
         gdal_polygonize.py "${FILEPATH_OUT_TMP}" "${FILEPATH_OUT_CLEAN}" -q
