d28e3e5a0b2a77d9600f646573636242a35d72c4
Nicholas Fabina <nicholas.fabina@gmail.com>
2019-09-12 15:06:54 -0500

    Adds warning to run script.

--- gcrmnbc/download_and_clean_all_data.sh
+++ gcrmnbc/download_and_clean_all_data.sh
@@ -4,6 +4,13 @@
 set -e
 
 echo "Download and clean all data"
+echo "Currently, it takes hours/days to parse the shapefiles for raster quads. Do you want to continue?"
+select yn in "Yes" "No"; do
+    case $yn in
+        Yes ) break;;
+        No ) exit;;
+    esac
+done
 
 echo "Download response geojson"
 ./data_acquisition_responses/download_response_geojson_from_vulcan.sh
