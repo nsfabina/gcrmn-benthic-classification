dff0794b7b32233697822e06b5dd41104abdd5f6
Nicholas Fabina <nicholas.fabina@gmail.com>
2019-09-16 11:18:54 -0500

    Adds new scripts to run script, fixes python vs shell calls.

--- gcrmnbc/download_and_clean_all_data.sh
+++ gcrmnbc/download_and_clean_all_data.sh
@@ -16,13 +16,16 @@ echo "Download response geojson"
 ./data_acquisition_responses/download_response_geojson_from_vulcan.sh
 
 echo "Create shapefile 'quads' for responses to determine which feature quads are needed"
-./data_cleaning/create_response_shapefile_quads.py
+python ./data_cleaning/create_response_shapefile_quads.py
 
 echo "Download feature quads"
-./data_acquisition_features/download_feature_quads.py
+python ./data_acquisition_features/download_feature_quads.py
 
 echo "Rasterize response shapefiles according to feature extents"
-./data_cleaning/rasterize_response_quads.py
+python ./data_cleaning/rasterize_response_quads.py
+
+echo "Create shapefile boundaries for training data sampling"
+python ./data_cleaning/create_sampling_boundary_shapefiles.py
 
 echo "Download UNEP evaluation data"
 ./data_acquisition_evaluation/download_unep_from_fabina_gdrive.sh
