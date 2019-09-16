6b5cdeacbdeff0d1822c72a7850acb2acb0a59b6
Nicholas Fabina <nicholas.fabina@gmail.com>
2019-09-16 11:16:35 -0500

    Adds encodings file.

---
+++ gcrmnbc/utils/encodings.py
@@ -0,0 +1,15 @@
+LAND = 'Land'
+WATER = 'Deep Reef Water 10m+'
+REEF_TOP = 'Reef Top'
+NOT_REEF_TOP = 'Not Reef Top'
+CLOUD_SHADE = 'Cloud-Shade'
+UNKNOWN = 'Unknown'
+
+MAPPINGS = {
+    LAND: 1,
+    WATER: 2,
+    REEF_TOP: 3,
+    NOT_REEF_TOP: 4,
+    CLOUD_SHADE: 5,
+    UNKNOWN: 6,
+}
