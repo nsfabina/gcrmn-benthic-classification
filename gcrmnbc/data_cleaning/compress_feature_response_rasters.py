e3065042dd8c7593b1dadced46a8d7db900c707a
Nicholas Fabina <nicholas.fabina@gmail.com>
2019-09-17 09:17:39 -0500

    Handles gdaltranslate not overwritting via Python.

--- gcrmnbc/data_cleaning/compress_feature_response_rasters.py
+++ gcrmnbc/data_cleaning/compress_feature_response_rasters.py
@@ -1,5 +1,6 @@
 import logging
 import os
+import shutil
 import sys
 
 import gdal
@@ -16,17 +17,22 @@ _handler.setFormatter(_formatter)
 _logger.addHandler(_handler)
 
 
-DIR_DATA = '/scratch/nfabina/gcrmn-benthic-classification/training_data/clean'
+DIR_BASE = '/scratch/nfabina/gcrmn-benthic-classification/training_data'
+DIR_CLEAN = os.path.join(DIR_BASE, 'clean')
+DIR_TMP = os.path.join(DIR_BASE, 'tmp')
 
 
 def compress_feature_response_rasters() -> None:
     _logger.info('Compress feature rasters')
-    filepaths_features = [os.path.join(DIR_DATA, fn) for fn in os.listdir(DIR_DATA) if fn.endswith('.tif')]
-    for idx_filepath, filepath in enumerate(filepaths_features):
+    filepaths_features = [os.path.join(DIR_CLEAN, fn) for fn in os.listdir(DIR_CLEAN) if fn.endswith('.tif')]
+    filepath_tmp = os.path.join(DIR_TMP, 'tmp_compress.tif')
+    for idx_filepath, filepath_clean in enumerate(filepaths_features):
         _logger.debug('Compressing raster {} of {}:  {}'.format(
-            1+idx_filepath, len(filepaths_features), filepath))
+            1+idx_filepath, len(filepaths_features), filepath_clean))
         options_translate = gdal.TranslateOptions(creationOptions=['COMPRESS=DEFLATE', 'TILED=YES'])
-        gdal.Translate(filepath, filepath, options=options_translate)
+        gdal.Translate(filepath_tmp, filepath_clean, options=options_translate)
+        shutil.copy(filepath_tmp, filepath_clean)
+        os.remove(filepath_tmp)
 
 
 if __name__ == '__main__':
