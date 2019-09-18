e6e6e7bef09614bbacbd334643dbf641d40bc58e
Nicholas Fabina <nicholas.fabina@gmail.com>
2019-09-18 10:47:33 -0500

    Dynamically adds land and water files to config.

--- gcrmnbc/utils/shared_configs.py
+++ gcrmnbc/utils/shared_configs.py
@@ -10,9 +10,13 @@ _DIR_DATA_BASE = '/scratch/nfabina/gcrmn-benthic-classification/'
 _DIR_DATA_CLEAN = os.path.join(_DIR_DATA_BASE, 'training_data/clean')
 _DIR_DATA_BUILT = os.path.join(_DIR_DATA_BASE, 'built_{}_{}')
 
-_SUFFIX_FEATURES = 'features.tif'
-_SUFFIX_RESPONSES = 'responses.tif'
-_SUFFIX_BOUNDARIES = 'boundaries.shp'
+_SUFFIX_FEATURES = '_features.tif'
+
+_SUFFIXES_RESPONSES_BOUNDARIES = (
+    ('_responses.tif', '_boundaries.shp', True),  # Reef data, True/False denotes whether its required or not
+    ('_land.tif', '_land.shp', False),  # Land data
+    ('_water.tif', '_water.shp', False),  # Water data
+)
 
 _RESPONSE_MAPPINGS = ('lwr', )
 _RESPONSE_MAPPING_CLASSES = {'lwr': 4, }
@@ -21,15 +25,40 @@ _RESPONSE_MAPPING_CLASSES = {'lwr': 4, }
 def build_dynamic_config(filepath_config: str, response_mapping: str) -> configs.Config:
     assert response_mapping in _RESPONSE_MAPPINGS, \
         'response_mapping is {} but must be one of:  {}'.format(response_mapping, _RESPONSE_MAPPINGS)
-    # Get all feature, response, and boundary files
-    filepaths_features = [[os.path.join(_DIR_DATA_CLEAN, filename)] for filename in os.listdir(_DIR_DATA_CLEAN)
-                          if filename.endswith(_SUFFIX_FEATURES)]
-    filepaths_responses = [[re.sub(_SUFFIX_FEATURES, _SUFFIX_RESPONSES, filepath[0])] for filepath in filepaths_features]
-    filepaths_boundaries = [re.sub(_SUFFIX_FEATURES, _SUFFIX_BOUNDARIES, filepath[0]) for filepath in filepaths_features]
-    missing_features = [filepath[0] for filepath in filepaths_responses if not os.path.exists(filepath[0])]
-    missing_boundaries = [filepath for filepath in filepaths_boundaries if not os.path.exists(filepath)]
-    assert not missing_features, 'Not all response files are present: {}'.format(missing_features)
-    assert not missing_boundaries, 'Not all boundary files are present: {}'.format(missing_boundaries)
+
+    filepaths_features = list()
+    filepaths_responses = list()
+    filepaths_boundaries = list()
+
+    # Note that we need to use feature files multiple times in some cases. Feature files will always be associated with
+    # reef training data, but may also be associated with land and/or water training data. Thus, feature files may be
+    # used 1-3 times in the training data.
+
+    for filename in os.listdir(_DIR_DATA_CLEAN):
+        if not filename.endswith(_SUFFIX_FEATURES):
+            continue
+
+        filepath_features = os.path.join(_DIR_DATA_CLEAN, filename)
+
+        for suffix_responses, suffix_boundaries, is_required in _SUFFIXES_RESPONSES_BOUNDARIES:
+            filepath_responses = re.sub(_SUFFIX_FEATURES, suffix_responses, filepath_features)
+            filepath_boundaries = re.sub(_SUFFIX_FEATURES, suffix_boundaries, filepath_features)
+
+            if is_required:
+                assert os.path.exists(filepath_responses), \
+                    'Response file is not available at {}'.format(filepath_responses)
+                assert os.path.exists(filepath_boundaries), \
+                    'Boundary file is not available at {}'.format(filepath_boundaries)
+            if os.path.exists(filepath_responses):
+                assert os.path.exists(filepath_boundaries), \
+                    'Response file is available, but boundary file not found:  {} and {}'.format(
+                        filepath_responses, filepath_boundaries)
+            if not os.path.exists(filepath_responses):
+                continue
+
+            filepaths_features.append([filepath_features])
+            filepaths_responses.append([filepath_responses])
+            filepaths_boundaries.append(filepath_boundaries)
 
     # Parse config
     config = configs.create_config_from_file(filepath_config)
