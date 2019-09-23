1b3a647d63edfa871cf0d9aecc5931c321d3268c
nsfabina <nicholas.fabina@gmail.com>
2019-09-23 09:57:38 -0700

    Fixes misc.

--- gcrmnbc/model_training/remove_classification_locks.sh
+++ gcrmnbc/model_training/remove_classification_locks.sh
@@ -3,8 +3,8 @@
 
 DIR_MODELS="../models"
 
-for MODEL in `ls models`; do
-  for RESPONSE_MAPPING in `ls ../models/${MODEL}`; do
+for MODEL in `ls "${DIR_MODELS}"`; do
+  for RESPONSE_MAPPING in `ls "${DIR_MODELS}/${MODEL}"`; do
     LOCK="${DIR_MODELS}/${MODEL}/${RESPONSE_MAPPING}/classify.lock"
     JOB_NAME="classify_${MODEL}_${RESPONSE_MAPPING}"
     FOUND=$(squeue -u nfabina -o %j | grep ${JOB_NAME})
