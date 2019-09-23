4a7271bb953275f97767dd4c96957e322e6eb84b
nsfabina <nicholas.fabina@gmail.com>
2019-09-23 10:17:01 -0700

    Fixes writing multipolys.

--- gcrmnbc/data_cleaning/create_evaluation_reef_multipolygons.py
+++ gcrmnbc/data_cleaning/create_evaluation_reef_multipolygons.py
@@ -1,3 +1,4 @@
+from collections import OrderedDict
 import os
 
 import fiona
@@ -16,12 +17,12 @@ PATH_REEF_MULTIPOLY = 'clean/reef_outline_union.shp'
 
 SHAPEFILE_DRIVER = 'ESRI Shapefile'
 SHAPEFILE_EPSG = 3857
-SHAPEFILE_SCHEMA = {'geometry': 'Polygon', }
+SHAPEFILE_SCHEMA = {'geometry': 'Polygon', 'properties': OrderedDict([('class_code', 'int')])}
 
 
 def create_uq_reef_multipolygons_for_model_evaluation() -> None:
     _logger.info('Create UQ reef multipolygons for model evaluation')
-    dirs_reefs = os.listdir(DIR_EVAL_DATA)
+    dirs_reefs = sorted(os.listdir(DIR_EVAL_DATA))
     crs = fiona.crs.from_epsg(3857)
     for dir_reef in dirs_reefs:
         _logger.debug('Create multipolygon for reef {}'.format(dir_reef))
@@ -32,7 +33,7 @@ def create_uq_reef_multipolygons_for_model_evaluation() -> None:
         reef = shapely.ops.unary_union([shapely.geometry.shape(feature['geometry']) for feature in features])
         _logger.debug('Writing multipolygon to file at {}'.format(filepath_out))
         with fiona.open(filepath_out, 'w', driver=SHAPEFILE_DRIVER, crs=crs, schema=SHAPEFILE_SCHEMA) as file_:
-            file_.write(reef)
+            file_.write({'geometry': shapely.geometry.mapping(reef), 'properties': {'class_code': 1}})
 
 
 if __name__ == '__main__':
