d48f88a44ef781eb58c0068550ec7c1e4e637f36
Nicholas Fabina <nicholas.fabina@gmail.com>
2019-10-01 14:50:28 -0500

    Fixes filter bug.

--- gcrmnbc/data_cleaning/downsample_rasters.py
+++ gcrmnbc/data_cleaning/downsample_rasters.py
@@ -16,7 +16,7 @@ _DOWNSAMPLE_PCTS = (50, 25)
 
 def downsample_rasters() -> None:
     _logger.info('Downsample rasters')
-    filenames = [filename for filename in os.listdir(DIR_DATA_CLEAN) if not filename.endswith('.tif')]
+    filenames = [filename for filename in os.listdir(DIR_DATA_CLEAN) if filename.endswith('.tif')]
 
     for filename in tqdm(filenames, desc='Downsampling rasters'):
         filepath_in = os.path.join(DIR_DATA_CLEAN, filename)
