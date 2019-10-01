d8f84e318e949b6f3c157a0dc803fcab1f835896
nsfabina <nicholas.fabina@gmail.com>
2019-10-01 12:00:07 -0700

    Fixes run command.

--- gcrmnbc/utils/shared_submit_slurm.py
+++ gcrmnbc/utils/shared_submit_slurm.py
@@ -1,4 +1,3 @@
-SLURM_COMMAND = 'sbatch --mail-user=nsfabina@asu.edu --nodes=1 --cpus-per-task=1 --ntasks=1 --mem-per-cpu=20000 ' + \
-                '--qos=wildfire '
-SLURM_GPUS = '--partition gpu --gres=gpu:1 '
+SLURM_COMMAND = 'sbatch --mail-user=nsfabina@asu.edu --nodes=1 --cpus-per-task=1 --ntasks=1 --mem-per-cpu=20000 '
+SLURM_GPUS = '--partition gpu --gres=gpu:1 --qos=wildfire '
 SLURM_GPUS_LARGE = SLURM_GPUS + '--constraint="V100_32" '
