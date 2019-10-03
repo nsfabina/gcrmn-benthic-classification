0648f5c1835be07c06999bdc2c778ebc180cbf75
Nicholas Fabina <nicholas.fabina@gmail.com>
2019-10-03 16:59:38 -0500

    Fixes downsampling script to make more legitimate.

--- gcrmnbc/data_cleaning/downsample_rasters.py
+++ gcrmnbc/data_cleaning/downsample_rasters.py
@@ -3,27 +3,24 @@ import re
 
 from tqdm import tqdm
 
-from gcrmnbc.utils import gdal_command_line, logs
+from gcrmnbc.utils import gdal_command_line, logs, paths
 
 
 _logger = logs.get_logger(__file__)
-
-DIR_DATA = '/scratch/nfabina/gcrmn-benthic-classification/training_data'
-DIR_DATA_CLEAN = os.path.join(DIR_DATA, 'clean')
 
 _DOWNSAMPLE_PCTS = (50, 25)
 
 
 def downsample_rasters() -> None:
     _logger.info('Downsample rasters')
-    filenames = [filename for filename in os.listdir(DIR_DATA_CLEAN) if filename.endswith('.tif')]
+    filenames = [filename for filename in os.listdir(paths.DIR_DATA_TRAIN_CLEAN) if filename.endswith('.tif')]
 
     for filename in tqdm(filenames, desc='Downsampling rasters'):
-        filepath_in = os.path.join(DIR_DATA_CLEAN, filename)
+        filepath_in = os.path.join(paths.DIR_DATA_TRAIN_CLEAN, filename)
         resampling = 'bilinear' if re.search('features', filename) else 'nearest'
 
         for pct in _DOWNSAMPLE_PCTS:
-            filepath_out = os.path.join(DIR_DATA, 'downsample_{}'.format(pct), filename)
+            filepath_out = os.path.join(paths.DIR_DATA_TRAIN, 'downsample_{}'.format(pct), filename)
             if not os.path.exists(os.path.dirname(filepath_out)):
                 os.makedirs(os.path.dirname(filepath_out))
 
