9751d419f9ce467a7236e8bc5f22d91f7aa96b36
Nicholas Fabina <nicholas.fabina@gmail.com>
2019-10-04 09:35:36 -0500

    Updates report for many models.

--- gcrmnbc/application_calval/create_comparative_performance_report.py
+++ gcrmnbc/application_calval/create_comparative_performance_report.py
@@ -2,8 +2,9 @@ import argparse
 import json
 import os
 import re
-from typing import Union
+from typing import Callable, List, Tuple, Union
 
+from matplotlib.backends.backend_pdf import PdfPages
 import matplotlib.pyplot as plt
 import numpy as np
 
@@ -32,39 +33,43 @@ def create_comparative_performance_report(label_experiment: str, response_mappin
             continue
         with open(filepath_model_stats) as file_:
             asu_statistics[dir_config] = json.load(file_)
-    asu_statistics = sorted(asu_statistics.items())
+    asu_statistics = _sort_models_by_average_fscore(asu_statistics)
 
     _logger.debug('Create report')
     lines = list(['Comparative Reef Performance Report', '{} reef mappings'.format(response_mapping), '', ''])
 
     # Calculate and print overall performance
-    lines.append('------------------------------------------------------------------------------------------------')
     lines.append('')
     lines.append('  Performance Summary')
     lines.append('')
+    lines.append('      Mean F-score')
+    lines.append('')
+    lines.append('          {:<50}{:>30}'.format('UNEP:', _get_fscore_average_str(unep_statistics)))
+    for config_name, stats in asu_statistics:
+        lines.append('          {:<50}{:>30}'.format(config_name+':', _get_fscore_average_str(stats)))
+    lines.append('')
+
     lines.append('      Mean Recall:  actual reef area which is detected by the model')
     lines.append('')
-    lines.append('          {:<24}{}'.format('UNEP:', _get_recall_average_str(unep_statistics)))
+    lines.append('          {:<50}{:>30}'.format('UNEP:', _get_recall_average_str(unep_statistics)))
     for config_name, stats in asu_statistics:
-        lines.append('          {:<24}{}'.format(config_name+':', _get_recall_average_str(stats)))
+        lines.append('          {:<50}{:>30}'.format(config_name+':', _get_recall_average_str(stats)))
     lines.append('')
 
     lines.append('      Mean Precision:  model detections which are actually reef')
     lines.append('')
-    lines.append('          {:<24}{}'.format('UNEP:', _get_precision_average_str(unep_statistics)))
+    lines.append('          {:<50}{:>30}'.format('UNEP:', _get_precision_average_str(unep_statistics)))
     for config_name, stats in asu_statistics:
-        lines.append('          {:<24}{}'.format(config_name+':', _get_precision_average_str(stats)))
-    lines.append('')
-    lines.append('')
+        lines.append('          {:<50}{:>30}'.format(config_name+':', _get_precision_average_str(stats)))
+    pages = list([lines])
 
     # Create sections for each reef
     for reef, unep_stats in sorted(unep_statistics.items()):
+        lines = list()
         asu_reef_stats = [(config_name, stats.get(reef)) for config_name, stats in asu_statistics if stats.get(reef)]
         reef_name = re.sub('_', ' ', reef).title()
 
         # Header
-        lines.append('------------------------------------------------------------------------------------------------')
-        lines.append('')
         lines.append(reef_name)
         lines.append('')
         lines.append('')
@@ -72,16 +77,16 @@ def create_comparative_performance_report(label_experiment: str, response_mappin
         # Recall and precision
         lines.append('  Recall:  actual reef area which is detected by the model')
         lines.append('')
-        lines.append('      {:<24}{}'.format('UNEP:', _get_recall_str(unep_stats)))
+        lines.append('      {:<50}{:>30}'.format('UNEP:', _get_recall_str(unep_stats)))
         for config_name, stats in asu_reef_stats:
-            lines.append('      {:<24}{}'.format(config_name+':', _get_recall_str(stats)))
+            lines.append('      {:<50}{:>30}'.format(config_name+':', _get_recall_str(stats)))
         lines.append('')
 
         lines.append('  Precision:  model detections which are actually reef')
         lines.append('')
-        lines.append('      {:<24}{}'.format('UNEP:', _get_precision_str(unep_stats)))
+        lines.append('      {:<50}{:>30}'.format('UNEP:', _get_precision_str(unep_stats)))
         for config_name, stats in asu_reef_stats:
-            lines.append('      {:<24}{}'.format(config_name+':', _get_precision_str(stats)))
+            lines.append('      {:<50}{:>30}'.format(config_name+':', _get_precision_str(stats)))
         lines.append('')
         lines.append('')
 
@@ -94,71 +99,88 @@ def create_comparative_performance_report(label_experiment: str, response_mappin
         # Reef area and detections
         lines.append('  Reef area')
         lines.append('')
-        lines.append('      {:<24}{:8.1f} km2 | {:4.1f} %  of total area'.format(
-            'ACA:', unep_stats['groundtruth_reef_area'], 100*unep_stats['groundtruth_reef_pct']))
-        lines.append('      {:<24}{:8.1f} km2 | {:4.1f} %  of total area'.format(
-            'UNEP:', unep_stats['model_reef_area'], 100*unep_stats['model_reef_pct']))
+        string = '{:8.1f} km2 | {:4.1f} %  of total area'.format(
+            unep_stats['groundtruth_reef_area'], 100*unep_stats['groundtruth_reef_pct'])
+        lines.append('      {:<50}{:>30}'.format('ACA:', string))
+        string = '{:8.1f} km2 | {:4.1f} %  of total area'.format(
+            unep_stats['model_reef_area'], 100*unep_stats['model_reef_pct'])
+        lines.append('      {:<50}{:>30}'.format('UNEP:', string))
         for config_name, stats in asu_reef_stats:
-            lines.append('      {:<24}{:8.1f} km2 | {:4.1f} %  of total area'.format(
-                config_name+':', stats['model_reef_area'], 100*stats['model_reef_pct']))
+            string = '{:8.1f} km2 | {:4.1f} %  of total area'.format(
+                stats['model_reef_area'], 100*stats['model_reef_pct'])
+            lines.append('      {:<50}{:>30}'.format(config_name+':', string))
         lines.append('')
 
         lines.append('  Reef detections')
         lines.append('')
         lines.append('      True positives')
-        lines.append('          {:<24}{:8.1f} km2 | {:4.1f} %  of reef area'.format(
-            'UNEP:', unep_stats['area_tp'], 100*unep_stats['area_tp']/unep_stats['groundtruth_reef_area']))
+        string = '{:8.1f} km2 | {:4.1f} %  of reef area'.format(
+            unep_stats['area_tp'], 100*unep_stats['area_tp']/unep_stats['groundtruth_reef_area'])
+        lines.append('          {:<50}{:>30}'.format('UNEP:', string))
         for config_name, stats in asu_reef_stats:
-            lines.append('          {:<24}{:8.1f} km2 | {:4.1f} %  of reef area'.format(
-                config_name+':', stats['area_tp'], 100*stats['area_tp']/stats['groundtruth_reef_area']))
+            string = '{:8.1f} km2 | {:4.1f} %  of reef area'.format(
+                stats['area_tp'], 100*stats['area_tp']/stats['groundtruth_reef_area'])
+            lines.append('          {:<50}{:>30}'.format(config_name+':', string))
         lines.append('')
 
         lines.append('      False positives')
-        lines.append('          {:<24}{:8.1f} km2 | {:4.1f} %  of non-reef area'.format(
-            'UNEP:', unep_stats['area_fp'], 100*unep_stats['area_fp']/unep_stats['groundtruth_nonreef_area']))
+        string = '{:8.1f} km2 | {:4.1f} %  of reef area'.format(
+            unep_stats['area_fp'], 100*unep_stats['area_fp']/unep_stats['groundtruth_nonreef_area'])
+        lines.append('          {:<50}{:>30}'.format('UNEP:', string))
         for config_name, stats in asu_reef_stats:
-            lines.append('          {:<24}{:8.1f} km2 | {:4.1f} %  of non-reef area'.format(
-                config_name+':', stats['area_fp'], 100*stats['area_fp']/stats['groundtruth_nonreef_area']))
+            string = '{:8.1f} km2 | {:4.1f} %  of reef area'.format(
+                stats['area_fp'], 100*stats['area_fp']/stats['groundtruth_nonreef_area'])
+            lines.append('          {:<50}{:>30}'.format(config_name+':', string))
         lines.append('')
         lines.append('')
 
         # Non-reef area and detections
         lines.append('  Non-reef area')
         lines.append('')
-        lines.append('      {:<24}{:8.1f} km2 | {:4.1f} %  of total area'.format(
-            'ACA:', unep_stats['groundtruth_nonreef_area'], 100*unep_stats['groundtruth_nonreef_pct']))
-        lines.append('      {:<24}{:8.1f} km2 | {:4.1f} %  of total area'.format(
-            'UNEP:', unep_stats['model_nonreef_area'], 100*unep_stats['model_nonreef_pct']))
+        string = '{:8.1f} km2 | {:4.1f} %  of total area'.format(
+            unep_stats['groundtruth_nonreef_area'], 100*unep_stats['groundtruth_nonreef_pct'])
+        lines.append('      {:<50}{:>30}'.format('ACA:', string))
+        string = '{:8.1f} km2 | {:4.1f} %  of total area'.format(
+            unep_stats['model_nonreef_area'], 100*unep_stats['model_nonreef_pct'])
+        lines.append('      {:<50}{:>30}'.format('UNEP:', string))
         for config_name, stats in asu_reef_stats:
-            lines.append('      {:<24}{:8.1f} km2 | {:4.1f} %  of total area'.format(
-                config_name+':', stats['model_nonreef_area'], 100*stats['model_nonreef_pct']))
+            string = '{:8.1f} km2 | {:4.1f} %  of total area'.format(
+                stats['model_nonreef_area'], 100*stats['model_nonreef_pct'])
+            lines.append('      {:<50}{:>30}'.format(config_name+':', string))
         lines.append('')
 
         lines.append('  Non-reef detections')
         lines.append('')
         lines.append('      True negatives')
-        lines.append('          {:<24}{:8.1f} km2 | {:4.1f} %  of reef area'.format(
-            'UNEP:', unep_stats['area_tn'], 100*unep_stats['area_tn']/unep_stats['groundtruth_nonreef_area']))
+        string = '{:8.1f} km2 | {:4.1f} %  of reef area'.format(
+            unep_stats['area_tn'], 100*unep_stats['area_tn']/unep_stats['groundtruth_nonreef_area'])
+        lines.append('          {:<50}{:>30}'.format('UNEP:', string))
         for config_name, stats in asu_reef_stats:
-            lines.append('          {:<24}{:8.1f} km2 | {:4.1f} %  of reef area'.format(
-                config_name+':', stats['area_tn'], 100*stats['area_tn']/stats['groundtruth_nonreef_area']))
+            string = '{:8.1f} km2 | {:4.1f} %  of reef area'.format(
+                stats['area_tn'], 100*stats['area_tn']/stats['groundtruth_nonreef_area'])
+            lines.append('          {:<50}{:>30}'.format(config_name+':', string))
         lines.append('')
 
         lines.append('      False negatives')
-        lines.append('          {:<24}{:8.1f} km2 | {:4.1f} %  of reef area'.format(
-            'UNEP:', unep_stats['area_fn'], 100*unep_stats['area_fn']/unep_stats['groundtruth_reef_area']))
+        string = '{:8.1f} km2 | {:4.1f} %  of reef area'.format(
+            unep_stats['area_fn'], 100*unep_stats['area_fn']/unep_stats['groundtruth_reef_area'])
+        lines.append('          {:<50}{:>30}'.format('UNEP:', string))
         for config_name, stats in asu_reef_stats:
-            lines.append('          {:<24}{:8.1f} km2 | {:4.1f} %  of reef area'.format(
-                config_name+':', stats['area_fn'], 100*stats['area_fn']/stats['groundtruth_reef_area']))
+            string = '{:8.1f} km2 | {:4.1f} %  of reef area'.format(
+                stats['area_fn'], 100*stats['area_fn']/stats['groundtruth_reef_area'])
+            lines.append('          {:<50}{:>30}'.format(config_name+':', string))
         lines.append('')
         lines.append('')
 
     # Finalize figure and save
-    height_per_line = 0.145
-    fig, ax = plt.subplots(figsize=(8.5, 2.0 + height_per_line * len(lines)))
-    ax.text(0, 0, '\n'.join(lines), **{'fontsize': 8, 'fontfamily': 'monospace'})
-    ax.axis('off')
-    plt.savefig(os.path.join(dir_experiment, _FILENAME_FIG_OUT.format(label_experiment, response_mapping)))
+    filename_out = os.path.join(dir_experiment, _FILENAME_FIG_OUT.format(label_experiment, response_mapping))
+    with PdfPages(filename_out) as pdf:
+        height_per_line = 0.145
+        for page in pages:
+            fig, ax = plt.subplots(figsize=(8.5, 2.0 + height_per_line * len(page)))
+            ax.text(0, 0, '\n'.join(lines), **{'fontsize': 8, 'fontfamily': 'monospace'})
+            ax.axis('off')
+            pdf.savefig(fig)
 
 
 def _get_precision_str(reef_statistics: dict) -> str:
@@ -183,32 +205,53 @@ def _get_recall_str(reef_statistics: dict) -> str:
     return recall_str
 
 
+def _get_fscore_average_str(model_statistics: dict) -> str:
+    return _get_stat_average_str(model_statistics, _calculate_fscore)
+
+
 def _get_precision_average_str(model_statistics: dict) -> str:
-    precisions = list()
-    weights = list()
-    for reef_statistics in model_statistics.values():
-        precision = _calculate_precision(reef_statistics)
-        if precision is None:
-            continue
-        precisions.append(precision)
-        weights.append(reef_statistics['groundtruth_reef_area'])
-    unweighted = np.mean(precisions)
-    weighted = np.average(precisions, weights=weights)
-    return '{:4.1f} %  |  {:4.1f} %  weighted by area'.format(unweighted, weighted)
+    return _get_stat_average_str(model_statistics, _calculate_precision)
 
 
 def _get_recall_average_str(model_statistics: dict) -> str:
-    recalls = list()
+    return _get_stat_average_str(model_statistics, _calculate_recall)
+
+
+def _get_stat_average_str(model_statistics: dict, calculator: Callable) -> str:
+    values = list()
     weights = list()
     for reef_statistics in model_statistics.values():
-        recall = _calculate_recall(reef_statistics)
-        if recall is None:
+        value = calculator(reef_statistics)
+        if value is None:
             continue
-        recalls.append(recall)
+        values.append(value)
         weights.append(reef_statistics['groundtruth_reef_area'])
-    unweighted = np.mean(recalls)
-    weighted = np.average(recalls, weights=weights)
-    return '{:4.1f} %  |  {:4.1f} %  weighted by area'.format(unweighted, weighted)
+    unweighted = np.mean(values)
+    weighted = np.average(values, weights=weights)
+    return '{:4.1f} %  |  {:4.1f} %  by area'.format(unweighted, weighted)
+
+
+def _sort_models_by_average_fscore(all_statistics: dict) -> List[Tuple[str, dict]]:
+    fscores = dict()
+    for model_name, model_stats in all_statistics.items():
+        values = list()
+        weights = list()
+        for reef_statistics in model_stats.values():
+            values.append(_calculate_fscore(reef_statistics))
+            weights.append(reef_statistics['groundtruth_reef_area'])
+        if any([v is None for v in values]):
+            continue
+        fscores[model_name] = np.average(values, weights=weights)
+    names_sorted = [name for name, score in sorted(fscores.items(), key=lambda x: x[1])]
+    return [(model_name, all_statistics[model_name]) for model_name in names_sorted]
+
+
+def _calculate_fscore(reef_statistics: dict) -> Union[float, None]:
+    precision = _calculate_precision(reef_statistics)
+    recall = _calculate_recall(reef_statistics)
+    if precision is None or recall is None:
+        return None
+    return 2 * precision * recall / (precision + recall)
 
 
 def _calculate_precision(reef_statistics: dict) -> Union[float, None]:
