2e76d4e51b039543ee73f72586813966e5fd79cd
Nicholas Fabina <nicholas.fabina@gmail.com>
2019-10-04 16:01:24 -0500

    Fixes config creation for different label experiments.

--- gcrmnbc/utils/shared_configs.py
+++ gcrmnbc/utils/shared_configs.py
@@ -6,14 +6,6 @@ from bfgn.configuration import configs
 from gcrmnbc.utils import paths
 
 
-_SUFFIX_FEATURES = '_features.tif'
-
-_SUFFIXES_RESPONSES_BOUNDARIES = (
-    ('_responses_{}b.tif', '_boundaries.shp'),  # Reef data
-    ('_land.tif', '_land.shp'),  # Land data
-    ('_water.tif', '_water.shp'),  # Water data
-)
-
 _RESPONSE_MAPPINGS = ('lwr', 'lwrn', )
 _RESPONSE_MAPPING_CLASSES = {'lwr': 6, 'lwrn': 10, }
 
@@ -29,26 +21,41 @@ def build_dynamic_config(config_name: str, label_experiment: str, response_mappi
     # Note that we need to use feature files multiple times in some cases. Feature files will always be associated with
     # reef training data, but may also be associated with land and/or water training data. Thus, feature files may be
     # used 1-3 times in the training data.
+    # Note that we need to grab land and water from the "clean" training data directory, and we need to grab general
+    # response data from the appropriate folder
 
+    # Get land or water data
     for filename in os.listdir(paths.DIR_DATA_TRAIN_CLEAN):
-        if not filename.endswith(_SUFFIX_FEATURES):
+        if not filename.endswith('_land.tif') and not filename.endswith('_water.tif'):
             continue
+        filepath_responses = os.path.join(paths.DIR_DATA_TRAIN_CLEAN, filename)
+        filepath_features = re.sub('_\w*.tif', '_features.tif', filepath_responses)
+        filepath_boundaries = re.sub('.tif', '.shp', filepath_responses)
+        assert os.path.exists(filepath_features), 'Features file not found:  {}'.format(filepath_features)
+        assert os.path.exists(filepath_boundaries), 'Boundaries file not found:  {}'.format(filepath_boundaries)
 
-        filepath_features = os.path.join(paths.DIR_DATA_TRAIN_CLEAN, filename)
+        filepaths_features.append(filepath_features)
+        filepaths_responses.append(filepath_responses)
+        filepaths_boundaries.append(filepath_boundaries)
 
-        for suffix_responses, suffix_boundaries in _SUFFIXES_RESPONSES_BOUNDARIES:
-            if 'responses' in suffix_responses:
-                filepath_responses = re.sub(
-                    _SUFFIX_FEATURES, suffix_responses.format(response_mapping), filepath_features)
-            else:
-                filepath_responses = re.sub(_SUFFIX_FEATURES, suffix_responses, filepath_features)
-            filepath_boundaries = re.sub(_SUFFIX_FEATURES, suffix_boundaries, filepath_features)
+    # Get regular response data
+    response_suffix = '_responses_{}b.tif'.format(response_mapping)
+    dir_data = os.path.join(paths.DIR_DATA_TRAIN, label_experiment)
+    assert os.path.exists(dir_data), 'Training data directory not found for label_experiment {}:  {}'.format(
+        label_experiment, dir_data)
+    for filename in os.listdir(dir_data):
+        if not filename.endswith(response_suffix):
+            continue
+        filepath_responses = os.path.join(dir_data, filename)
+        filepath_features = re.sub(response_suffix, '_features.tif', filepath_responses)
+        filename_boundaries = re.sub(response_suffix, '_boundaries.shp', filename)
+        filepath_boundaries = os.path.join(paths.DIR_DATA_TRAIN_CLEAN, filename_boundaries)
+        assert os.path.exists(filepath_features), 'Features file not found:  {}'.format(filepath_features)
+        assert os.path.exists(filepath_boundaries), 'Boundaries file not found:  {}'.format(filepath_boundaries)
 
-            if not os.path.exists(filepath_responses):
-                continue
-            filepaths_features.append([filepath_features])
-            filepaths_responses.append([filepath_responses])
-            filepaths_boundaries.append(filepath_boundaries)
+        filepaths_features.append([filepath_features])
+        filepaths_responses.append([filepath_responses])
+        filepaths_boundaries.append(filepath_boundaries)
 
     # Parse config and update dynamic values
     config = configs.create_config_from_file(paths.get_filepath_config(config_name))
@@ -58,28 +65,4 @@ def build_dynamic_config(config_name: str, label_experiment: str, response_mappi
     config.data_build.dir_out = paths.get_dir_built_data_experiment(label_experiment, response_mapping, config)
     config.model_training.dir_out = paths.get_dir_model_experiment_config(label_experiment, response_mapping, config)
     config.architecture.n_classes = _RESPONSE_MAPPING_CLASSES[response_mapping]
-
-    # Modify configs for different experiments
-    if label_experiment.startswith('downsample'):
-        config = _modify_config_for_downsampling(label_experiment, response_mapping, config)
-
-    return config
-
-
-def _modify_config_for_downsampling(label_experiment: str, response_mapping: str, config: configs.Config) \
-        -> configs.Config:
-    if label_experiment == 'downsample_25':
-        downsample_pct = '25'
-    elif label_experiment == 'downsample_50':
-        downsample_pct = '50'
-
-    # Modify feature and response filepaths, but leave boundaries the same
-    config.raw_files.feature_files = [
-        [re.sub('originals', 'downsample_{}'.format(downsample_pct), ff[0])] for ff in config.raw_files.feature_files]
-    config.raw_files.response_files = [
-        [re.sub('originals', 'downsample_{}'.format(downsample_pct), rf[0])] for rf in config.raw_files.response_files]
-
-    # Modify built data filepath
-    config.data_build.dir_out = paths.get_dir_built_data_experiment(label_experiment, response_mapping, config)
-    config.model_training.dir_out = paths.get_dir_model_experiment_config(label_experiment, response_mapping, config)
     return config
