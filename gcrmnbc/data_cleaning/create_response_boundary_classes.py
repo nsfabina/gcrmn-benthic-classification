8bb4d39326ebf544d633581698f128f53eea818b
Nicholas Fabina <nicholas.fabina@gmail.com>
2019-10-05 09:57:10 -0500

    Creates response boundaries for originals.

--- gcrmnbc/data_cleaning/create_response_boundary_classes.py
+++ gcrmnbc/data_cleaning/create_response_boundary_classes.py
@@ -16,21 +16,26 @@ _LABEL_EDGE = -8888
 
 def create_response_boundary_classes() -> None:
     _logger.info('Create response boundary classes')
-    dir_data = os.path.join(paths.DIR_DATA_TRAIN, 'downsample_50')
-    filepaths_srcs = [os.path.join(dir_data, fn) for fn in os.listdir(dir_data) if fn.endswith('responses_lwr.tif')]
-    for filepath_src in tqdm(sorted(filepaths_srcs), desc='Create boundary classes'):
-        _logger.debug('Processing file:  {}'.format(filepath_src))
-        filepath_dest = re.sub('.tif', 'b.tif', filepath_src)
-        _create_response_boundary_classes_for_filepath(filepath_src, filepath_dest)
+    subdirs_data = ('downsample_50', 'original')
+    thicknesses = (3, 4)
+    for subdir_data, thickness in zip(subdirs_data, thicknesses):
+        dir_data = os.path.join(paths.DIR_DATA_TRAIN, subdir_data)
+        filepaths_srcs = [os.path.join(dir_data, fn) for fn in os.listdir(dir_data) if fn.endswith('responses_lwr.tif')]
+        for filepath_src in tqdm(sorted(filepaths_srcs), desc='Create boundary classes for {}'.format(subdir_data)):
+            _logger.debug('Processing file:  {}'.format(filepath_src))
+            filepath_dest = re.sub('.tif', 'b.tif', filepath_src)
+            if os.path.exists(filepath_dest):
+                continue
+            _create_response_boundary_classes_for_filepath(filepath_src, filepath_dest, thickness)
 
 
-def _create_response_boundary_classes_for_filepath(filepath_src: str, filepath_dest: str) -> None:
+def _create_response_boundary_classes_for_filepath(filepath_src: str, filepath_dest: str, thickness: int) -> None:
     # Open original data
     raster_src = gdal.Open(filepath_src)
     band_src = raster_src.GetRasterBand(1)
     original = band_src.ReadAsArray()
     # Add boundaries
-    with_boundaries = _add_class_boundaries(original)
+    with_boundaries = _add_class_boundaries(original, thickness)
     # Write output file
     driver = raster_src.GetDriver()
     raster_dest = driver.Create(filepath_dest, raster_src.RasterXSize, raster_src.RasterYSize, 1, gdal.GDT_Int16)
@@ -42,14 +47,14 @@ def _create_response_boundary_classes_for_filepath(filepath_src: str, filepath_d
     del band_dest, band_src, raster_dest, raster_src
 
 
-def _add_class_boundaries(original: np.array) -> np.array:
+def _add_class_boundaries(original: np.array, thickness: int) -> np.array:
     # Calculate edges
     classes = (encodings.LAND, encodings.WATER, encodings.REEF_TOP, encodings.NOT_REEF_TOP)
     class_edges = dict()
     for class_ in classes:
         class_image = original.copy()
         class_image[class_image != encodings.MAPPINGS[class_]] = 0
-        class_edges[class_] = _get_edges(class_image)
+        class_edges[class_] = _get_edges(class_image, thickness)
     del class_image
     # Add class boundaries
     combinations = (
@@ -66,11 +71,10 @@ def _add_class_boundaries(original: np.array) -> np.array:
     return original
 
 
-def _get_edges(arr):
+def _get_edges(arr: np.array, thickness: int) -> np.array:
     edges = arr.copy()
-    edges[skimage.segmentation.find_boundaries(edges, connectivity=2, mode='thick')] = _LABEL_EDGE
-    edges[skimage.segmentation.find_boundaries(edges, connectivity=2, mode='thick')] = _LABEL_EDGE
-    edges[skimage.segmentation.find_boundaries(edges, connectivity=2, mode='thick')] = _LABEL_EDGE
+    for idx_thick in range(thickness):
+        edges[skimage.segmentation.find_boundaries(edges, connectivity=2, mode='thick')] = _LABEL_EDGE
     return edges
 
 
