618e4ddb5fcd1c0c779e906e808c54d5d8e90d4f
Nicholas Fabina <nicholas.fabina@gmail.com>
2019-10-07 13:54:55 -0500

    Adds script to downsample global quads.

---
+++ gcrmnbc/data_cleaning/downsample_global_feature_quads.py
@@ -0,0 +1,29 @@
+import os
+
+from tqdm import tqdm
+
+from gcrmnbc.utils import gdal_command_line, logs, paths
+
+
+_logger = logs.get_logger(__file__)
+
+
+_DOWNSAMPLE_PCT = 50
+_FILENAME_PREFIX = 'down{}'.format(_DOWNSAMPLE_PCT)
+
+
+def downsample_global_rasters() -> None:
+    _logger.info('Downsample rasters')
+    filenames = sorted([fn for fn in os.listdir(paths.DIR_DATA_GLOBAL) if not fn.startswith(_FILENAME_PREFIX)])
+
+    for filename in tqdm(filenames, desc='Downsampling rasters'):
+        filepath_in = os.path.join(paths.DIR_DATA_GLOBAL, filename)
+        filepath_out = os.path.join(paths.DIR_DATA_GLOBAL, _FILENAME_PREFIX + filename)
+        command = 'gdal_translate -outsize {pct}% {pct}% -r bilinear {path_in} {path_out}'.format(
+            pct=_DOWNSAMPLE_PCT, path_in=filepath_in, path_out=filepath_out)
+        gdal_command_line.run_gdal_command(command, _logger)
+        os.remove(filepath_in)
+
+
+if __name__ == '__main__':
+    downsample_global_rasters()
--- gcrmnbc/utils/paths.py
+++ gcrmnbc/utils/paths.py
@@ -20,6 +20,8 @@ DIR_DATA_EVAL = os.path.join(DIR_DATA, 'evaluation_data')
 DIR_DATA_APPLY_CALVAL = os.path.join(DIR_DATA, 'applied_data')
 DIR_DATA_APPLY_GLOBAL = os.path.join(DIR_DATA, 'tmp_global_application')
 
+DIR_DATA_GLOBAL = os.path.join(DIR_DATA, 'global_data')
+
 FILENAME_BUILD_COMPLETE = 'build.complete'
 FILENAME_CLASSIFY_LOCK = 'classify.lock'
 FILENAME_CLASSIFY_COMPLETE = 'classify.complete'
