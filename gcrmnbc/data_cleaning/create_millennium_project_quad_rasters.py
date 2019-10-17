0d4dd001de2da335a99e9e4f8a1feac4385c4366
nsfabina <nicholas.fabina@gmail.com>
2019-10-17 06:42:09 -0700

    Fixes to get raster creation running.

--- gcrmnbc/data_cleaning/create_millennium_project_quad_rasters.py
+++ gcrmnbc/data_cleaning/create_millennium_project_quad_rasters.py
@@ -23,9 +23,10 @@ def create_millennium_project_quad_rasters() -> None:
         filename_raster = re.sub('.shp', '_{}.tif', filename_poly)
         filepath_dest = os.path.join(paths.DIR_DATA_TRAIN_CLEAN_MP, filename_raster)
         quad_name = re.search(r'L15-\d{4}E-\d{4}N', filename_poly).group()
-        filepath_features = os.path.join(paths.DIR_DATA_TRAIN_CLEAN, quad_name + '_features.tif')
+        filepath_features = os.path.join(paths.DIR_DATA_TRAIN_RAW, quad_name + '_features.tif')
         if os.path.exists(filepath_dest):
             continue
+        assert os.path.exists(filepath_features), 'Features file not available at: {}'.format(filepath_features)
         if not os.path.exists(os.path.dirname(filepath_dest)):
             os.makedirs(os.path.dirname(filepath_dest))
 
@@ -39,27 +40,15 @@ def create_millennium_project_quad_rasters() -> None:
         lly = min([y0, y1])
         ury = max([y0, y1])
 
-<<<<<<< Updated upstream
         # Rasterize several attributes separately
         for attribute in ('L3_CODE', 'L4_CODE'):
             command = 'gdal_rasterize -ot Int16 -co COMPRESS=DEFLATE -co TILED=YES -a_nodata -9999 -init -9999 ' \
-                      '-a {attribute} -te {llx} {lly} {urx} {ury} -tr {xres} {yres} -a_srs {srs} {src} {dest}'
+                      '-a {attribute} -te {llx} {lly} {urx} {ury} -tr {xres} {yres} {src} {dest}'
             command = command.format(
                 attribute=attribute, llx=llx, lly=lly, urx=urx, ury=ury, xres=xres, yres=yres,
-                srs=srs, src=filepath_src, dest=filepath_dest.format(attribute)
-=======
-        # Rasterize each band for different attributes
-        for idx_band, property in enumerate(('L1_CODE', 'L2_CODE', 'L3_CODE', 'L4_CODE', 'DEPTH_CODE')):
-            command = \
-                'gdal_rasterize -ot Int16 -co COMPRESS=DEFLATE -co TILED=YES -a_nodata -9999 -init -9999 ' \
-                '{flag} {idx_band} -a {property} -te {llx} {lly} {urx} {ury} -tr {xres} {yres} {src} {dest}'
-            command = command.format(
-                flag='-b' if idx_band > 0 else '', idx_band=1+idx_band if idx_band > 0 else '', property=property, 
-                llx=llx, lly=lly, urx=urx, ury=ury, xres=xres, yres=yres, src=filepath_src, dest=filepath_dest
->>>>>>> Stashed changes
+                src=filepath_src, dest=filepath_dest.format(attribute)
             )
             gdal_command_line.run_gdal_command(command, _logger)
-        raise
 
 
 if __name__ == '__main__':
