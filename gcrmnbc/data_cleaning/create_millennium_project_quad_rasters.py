bc97eab7eb667c11f015b5880cfdcee769daf07f
nsfabina <nicholas.fabina@gmail.com>
2019-10-17 06:18:36 -0700

    Adds additional info to logging.

--- gcrmnbc/data_cleaning/create_millennium_project_quad_rasters.py
+++ gcrmnbc/data_cleaning/create_millennium_project_quad_rasters.py
@@ -26,10 +26,11 @@ def create_millennium_project_quad_rasters() -> None:
         filepath_features = os.path.join(paths.DIR_DATA_TRAIN_CLEAN, quad_name + '_features.tif')
         if os.path.exists(filepath_dest):
             continue
+        if not os.path.exists(os.path.dirname(filepath_dest)):
+            os.makedirs(os.path.dirname(filepath_dest))
 
         # Get rasterize parameters
         raster_features = gdal.Open(filepath_features)
-        srs = osr.SpatialReference(wkt=raster_features.GetProjection())
         cols = raster_features.RasterXSize
         rows = raster_features.RasterYSize
         llx, xres, _, y0, _, yres = raster_features.GetGeoTransform()
@@ -38,6 +39,7 @@ def create_millennium_project_quad_rasters() -> None:
         lly = min([y0, y1])
         ury = max([y0, y1])
 
+<<<<<<< Updated upstream
         # Rasterize several attributes separately
         for attribute in ('L3_CODE', 'L4_CODE'):
             command = 'gdal_rasterize -ot Int16 -co COMPRESS=DEFLATE -co TILED=YES -a_nodata -9999 -init -9999 ' \
@@ -45,8 +47,19 @@ def create_millennium_project_quad_rasters() -> None:
             command = command.format(
                 attribute=attribute, llx=llx, lly=lly, urx=urx, ury=ury, xres=xres, yres=yres,
                 srs=srs, src=filepath_src, dest=filepath_dest.format(attribute)
+=======
+        # Rasterize each band for different attributes
+        for idx_band, property in enumerate(('L1_CODE', 'L2_CODE', 'L3_CODE', 'L4_CODE', 'DEPTH_CODE')):
+            command = \
+                'gdal_rasterize -ot Int16 -co COMPRESS=DEFLATE -co TILED=YES -a_nodata -9999 -init -9999 ' \
+                '{flag} {idx_band} -a {property} -te {llx} {lly} {urx} {ury} -tr {xres} {yres} {src} {dest}'
+            command = command.format(
+                flag='-b' if idx_band > 0 else '', idx_band=1+idx_band if idx_band > 0 else '', property=property, 
+                llx=llx, lly=lly, urx=urx, ury=ury, xres=xres, yres=yres, src=filepath_src, dest=filepath_dest
+>>>>>>> Stashed changes
             )
             gdal_command_line.run_gdal_command(command, _logger)
+        raise
 
 
 if __name__ == '__main__':
--- gcrmnbc/utils/gdal_command_line.py
+++ gcrmnbc/utils/gdal_command_line.py
@@ -7,6 +7,7 @@ def run_gdal_command(command: str, logger: logging.Logger = None) -> None:
     completed = subprocess.run(shlex.split(command), capture_output=True)
     if completed.stderr:
         if logger:
+            logger.error('gdal command:  {}'.format(command))
             logger.error('gdal stdout:  {}'.format(completed.stdout.decode('utf-8')))
             logger.error('gdal stderr:  {}'.format(completed.stderr.decode('utf-8')))
     assert completed.returncode == 0, 'See above log lines for unknown error in gdal command:  {}'.format(command)
