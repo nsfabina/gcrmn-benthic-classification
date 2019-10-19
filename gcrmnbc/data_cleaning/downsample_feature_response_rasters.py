ed7db84c70494ceb822c60ca91a95995f3a19bfc
nsfabina <nicholas.fabina@gmail.com>
2019-10-19 11:07:14 -0700

    Fixes filepath parsing.

--- gcrmnbc/data_cleaning/downsample_feature_response_rasters.py
+++ gcrmnbc/data_cleaning/downsample_feature_response_rasters.py
@@ -29,7 +29,7 @@ def downsample_rasters() -> None:
         filepaths.append((filepath_in, filepath_out))
     # Aggregate MP files
     for filename in os.listdir(paths.DIR_DATA_TRAIN_CLEAN_MP):
-        if not filename.endswith('L3_CODE.tif') or not filename.endswith('L4_CODE.tif'):
+        if not filename.endswith('L3_CODE.tif') and not filename.endswith('L4_CODE.tif'):
             continue
         filepath_in = os.path.join(paths.DIR_DATA_TRAIN_CLEAN_MP, filename)
         filepath_out = os.path.join(dir_out_mp, filename)
