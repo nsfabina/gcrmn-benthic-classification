d74421d7cc96d9ebcc276445f65497d3b0b9317a
Nicholas Fabina <nicholas.fabina@gmail.com>
2019-11-13 10:50:49 -0600

    Modifies manifests to be more consise, but still too large.

--- gcrmnbc/earth_engine/upload_reef_products_to_earth_engine.py
+++ gcrmnbc/earth_engine/upload_reef_products_to_earth_engine.py
@@ -23,25 +23,28 @@ SAMPLE_LOCATIONS = {
 }
 
 MANIFEST_MOSAIC = {
-    'name': 'projects/earthengine-legacy/assets/users/nfabina/planet_mosaic/',
+    'name': 'projects/earthengine-legacy/assets/users/nfabina/planet_samples/',
     'tilesets': [{'sources': []}],
     'bands': [
         {'id': 'red', 'tileset_band_index': 0, 'pyramiding_policy': 'MEAN'},
         {'id': 'green', 'tileset_band_index': 1, 'pyramiding_policy': 'MEAN'},
         {'id': 'blue', 'tileset_band_index': 2, 'pyramiding_policy': 'MEAN'}
-    ]
+    ],
+    'uri_prefix': 'gs://coral-atlas-data-share/coral_reefs_2018_visual_v1_mosaic/',
 }
 
 MANIFEST_HEAT = {
-    'name': 'projects/earthengine-legacy/assets/users/nfabina/reef_probs/',
+    'name': 'projects/earthengine-legacy/assets/users/nfabina/reef_probs_v1/',
     'tilesets': [{'sources': []}],
-    'bands': [{'id': 'reef_probabilities', 'pyramiding_policy': 'MEAN'}]
+    'bands': [{'id': 'reef_probs', 'pyramiding_policy': 'MEAN'}],
+    'uri_prefix': 'gs://coral-atlas-data-share/gcrmn-global-map/',
 }
 
 MANIFEST_OUTLINE = {
-    'name': 'projects/earthengine-legacy/assets/users/nfabina/reef_preds/',
+    'name': 'projects/earthengine-legacy/assets/users/nfabina/reef_preds_v1/',
     'tilesets': [{'sources': []}],
-    'bands': [{'id': 'reef_predictions', 'pyramiding_policy': 'MODE'}]
+    'bands': [{'id': 'reef_preds', 'pyramiding_policy': 'MODE'}],
+    'uri_prefix': 'gs://coral-atlas-data-share/gcrmn-global-map/',
 }
 
 
@@ -50,13 +53,12 @@ def upload_reef_products_to_earth_engine(model_version: str) -> None:
     blobs_mosaic = data_bucket.get_imagery_quad_blobs()
     blobs_heat, blobs_outline = data_bucket.get_reef_heat_and_outline_quad_blobs(model_version)
     # Upload outlines
-    _create_asset_folders(model_version)
     asset_chunks = _create_asset_chunks(blobs_outline, include_global=True)
-    manifests = _create_manifests(asset_chunks, MANIFEST_OUTLINE, model_version)
+    manifests = _create_manifests(asset_chunks, MANIFEST_OUTLINE)
     _submit_uploads(manifests)
     # Upload heatmaps
     asset_chunks = _create_asset_chunks(blobs_heat, include_global=True)
-    manifests = _create_manifests(asset_chunks, MANIFEST_HEAT, model_version)
+    manifests = _create_manifests(asset_chunks, MANIFEST_HEAT)
     _submit_uploads(manifests)
     # Upload planet
     asset_chunks = _create_asset_chunks(blobs_mosaic, include_global=False)
@@ -73,36 +75,28 @@ def _create_asset_chunks(
         x_id = blob.x - blob.x % GRID_SIZE
         y_id = blob.y - blob.y % GRID_SIZE
         asset_id = '{:04d}E-{:04d}N'.format(x_id, y_id)
-        if asset_id in SAMPLE_LOCATIONS:
+        if asset_id in SAMPLE_LOCATIONS or include_global:
             asset_chunks.setdefault(asset_id, dict())[blob.quad_focal] = blob
-        if include_global:
-            asset_chunks.setdefault('global', dict())[blob.quad_focal] = blob
     return asset_chunks
 
 
 def _create_manifests(
         asset_chunks: Dict[str, Dict[str, data_bucket.QuadBlob]],
         manifest_template: dict,
-        model_version: str = None
 ) -> List[dict]:
     manifests = list()
     for asset_id, asset_blobs in asset_chunks.items():
         manifest = copy.deepcopy(manifest_template)
         name_suffix = asset_id
-        if model_version:
-            name_suffix = model_version + '/' + name_suffix
         manifest['name'] = manifest['name'] + name_suffix
-        uris = [{'uris': ['gs://coral-atlas-data-share/{}'.format(blob.blob.name)]} for blob in asset_blobs.values()]
+        uris = list()
+        for blob in asset_blobs.values():
+            blob_name = blob.blob.name
+            without_leading_path = '/'.join(blob_name.split('/')[1:])
+            uris.append({'uris': [without_leading_path]})
         manifest['tilesets'] = [{'sources': uris}]
         manifests.append(manifest)
     return manifests
-
-
-def _create_asset_folders(model_version: str) -> None:
-    base_command = 'earthengine create folder -p users/nfabina/{}'
-    for folder in ('planet_mosaic', 'reef_preds/{}'.format(model_version), 'reef_probs/{}'.format(model_version)):
-        command = base_command.format(folder, model_version)
-        gdal_command_line.run_gdal_command(command)
 
 
 def _submit_uploads(manifests: List[dict]) -> None:
