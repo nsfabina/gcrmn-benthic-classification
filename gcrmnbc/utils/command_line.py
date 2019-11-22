c7e4b070af7bae4551b00775773d5cf2951d9d20
Nicholas Fabina <nicholas.fabina@gmail.com>
2019-11-22 12:48:00 -0600

    Always prints command line error.

--- gcrmnbc/utils/command_line.py
+++ gcrmnbc/utils/command_line.py
@@ -6,8 +6,8 @@ import subprocess
 def run_command_line(command: str, logger: logging.Logger = None) -> None:
     completed = subprocess.run(shlex.split(command), capture_output=True)
     if completed.stderr:
+        message = 'gdal command:  {}\n\ngdal stdout:  {}\n\ngdal stderr:  {}'.format(
+            command, completed.stdout.decode('utf-8'), completed.stderr.decode('utf-8'))
         if logger:
-            logger.error('gdal command:  {}'.format(command))
-            logger.error('gdal stdout:  {}'.format(completed.stdout.decode('utf-8')))
-            logger.error('gdal stderr:  {}'.format(completed.stderr.decode('utf-8')))
-    assert completed.returncode == 0, 'See above log lines for unknown error in gdal command:  {}'.format(command)
+            logger.error(message)
+    assert completed.returncode == 0, message
