c9b0bb39aaaca5e19e5817a672bae6bc7e2fe701
Nicholas Fabina <nicholas.fabina@gmail.com>
2019-12-03 10:33:49 -0600

    Fixes to creating quad vrts.

--- gcrmnbc/data_cleaning/create_landsat_quad_vrts.py
+++ gcrmnbc/data_cleaning/create_landsat_quad_vrts.py
@@ -4,7 +4,6 @@ import re
 import gdal
 from tqdm import tqdm
 
-from gcrmnbc.utils import EPSG_DEST
 from gcrmnbc.utils import command_line, paths
 
 
@@ -18,20 +17,22 @@ def create_landsat_quad_vrts() -> None:
     if not os.path.exists(filepath_landsat_global):
         cw_dir = os.getcwd()
         os.chdir(paths.DIR_DATA_LANDSAT_ORIG)
-        filepaths_landsats = [f for f in os.listdir(paths.DIR_DATA_LANDSAT_ORIG) if re.search('\d{10}-\d{10}.tif$', f)]
+        filepaths_landsats = [f for f in os.listdir(paths.DIR_DATA_LANDSAT_ORIG) if re.search(r'\d{10}-\d{10}.tif$', f)]
         filepath_inputs = 'input_list.txt'
         with open(filepath_inputs, 'w') as file_:
             file_.writelines('\n'.join(filepaths_landsats))
-        command = 'gdalbuildvrt -a_srs EPSG:{srs_out} -input_file_list {filepath_inputs} {filepath_out}'.format(
-            srs_out=EPSG_DEST, filepath_inputs=filepath_inputs, filepath_out=filepath_landsat_global)
+        command = 'gdalbuildvrt -input_file_list {filepath_inputs} {filepath_out}'.format(
+            filepath_inputs=filepath_inputs, filepath_out=filepath_landsat_global)
         command_line.run_command_line(command)
         os.remove(filepath_inputs)
         os.chdir(cw_dir)
 
     # Create quad vrts
-    dir_out = os.path.join(paths.SUBDIR_DATA_TRAIN_DOWNSAMPLE.format(50))
+    dir_out = os.path.join(paths.DIR_DATA_LANDSAT, paths.SUBDIR_DATA_TRAIN_DOWNSAMPLE.format(50))
+    if not os.path.exists(dir_out):
+        os.makedirs(dir_out)
     for filepath_quad in tqdm(filepaths_quads):
-        filepath_out = os.path.join(dir_out, os.path.splitext(os.path.basename(filepath_quad))[0] + '_50.tif')
+        filepath_out = os.path.join(dir_out, os.path.basename(filepath_quad))
         # Get vrt parameters
         raster_quad = gdal.Open(filepath_quad)
         cols = raster_quad.RasterXSize
@@ -41,10 +42,10 @@ def create_landsat_quad_vrts() -> None:
         y1 = y0 + rows * yres
         lly = min([y0, y1])
         ury = max([y0, y1])
-        command = 'gdalbuildvrt -tr {xres} {yres} -te {llx} {lly} {urx} {ury} -vrtnodata -9999 -a_srs {srs_out} ' + \
+        command = 'gdalbuildvrt -tr {xres} {yres} -te {llx} {lly} {urx} {ury} -vrtnodata -9999 ' + \
                   '-r average {filepath_out} {filepath_src}'
         command = command.format(
-            xres=xres, yres=yres, llx=llx, lly=lly, urx=urx, ury=ury, srs_out=EPSG_DEST, filepath_out=filepath_out,
+            xres=xres, yres=-yres, llx=llx, lly=lly, urx=urx, ury=ury, filepath_out=filepath_out,
             filepath_src=filepath_landsat_global)
         command_line.run_command_line(command)
 
